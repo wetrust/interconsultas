0bd2780eeff21006de95801e14499262ee7303b0
Cristopher Castro <cristophernic@live.cl>
2019-03-23 20:39:21 -0300

    cargar datos correctos

--- application/controller/DashboardController.php
+++ application/controller/DashboardController.php
@@ -11,9 +11,9 @@ class DashboardController extends Controller
     public function index()
     {
         $this->View->render('dashboard/index', array(
-            'solicitudes_new' => SolicitudesModel::getAllSolicitudes(Session::get('user_email')),
+            'solicitudes_new' => SolicitudesModel::getAllNewSolicitudes(Session::get('user_email')),
             'solicitudes' => SolicitudesModel::getAllSolicitudes(Session::get('user_email')),
-            'solicitudes_old' => SolicitudesModel::getAllSolicitudes(Session::get('user_email'))
+            'solicitudes_old' => SolicitudesModel::getAllOldSolicitudes(Session::get('user_email'))
         ));
     }
 }
