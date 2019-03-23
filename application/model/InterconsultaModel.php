e5c10225e568e4504f474c741cc7d761836af0bf
Cristopher Castro <cristophernic@live.cl>
2019-03-23 20:25:46 -0300

    error nombre de modelo

--- application/model/InterconsultaModel.php
+++ application/model/InterconsultaModel.php
@@ -11,7 +11,7 @@ class InterconsultaModel
         if (isset($contrareferente->user_email)){
             if ($contrareferente->user_email == $solicitud_profesionalemail){
                 //almacenar la interconsulta
-                SolicitudModel::createSolicitud($solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_eg,$solicitud_eco,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional);
+                SolicitudesModel::createSolicitud($solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_eg,$solicitud_eco,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional);
                 
                 //enviar un email al médico contrareferente
                 EmailModel::sendContrareferenteEmail($solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_eg,$solicitud_eco,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional);
