50845ae570a232f9e59af71a2a13d6a9ba21a71b
Cristopher Castro <cristophernic@live.cl>
2019-03-28 18:33:33 -0300

    .

--- application/view/dashboard/agendar.php
+++ application/view/dashboard/agendar.php
@@ -67,12 +67,12 @@
                     </div>
                     <div class="col form-group">
                         <div class="form-check">
-                            <input type="radio" id="interconsulta.profesional.medico" value="Médico" name="interconsulta_profesional" class="form-check-input">
-                            <label class="form-check-label" disabled for="interconsulta.profesional.medico">Médico</label>
+                            <input type="radio" disabled id="interconsulta.profesional.medico" value="Médico" name="interconsulta_profesional" class="form-check-input">
+                            <label class="form-check-label" for="interconsulta.profesional.medico">Médico</label>
                         </div>
                         <div class="form-check">
-                            <input type="radio" id="interconsulta.profesional.matrona" value="Matrona" name="interconsulta_profesional" class="form-check-input">
-                            <label class="form-check-label" disabled for="interconsulta.profesional.matrona">Matrón/Matrona</label>
+                            <input type="radio" disabled id="interconsulta.profesional.matrona" value="Matrona" name="interconsulta_profesional" class="form-check-input">
+                            <label class="form-check-label" for="interconsulta.profesional.matrona">Matrón/Matrona</label>
                         </div>
                     </div>
                 </div>
