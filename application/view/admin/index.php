076ef2357f04ff584247e83e6fa40e0b0472e00e
Cristopher Castro <cristophernic@live.cl>
2019-04-03 17:47:40 -0300

    tabla de usuarios

--- application/view/admin/index.php
+++ application/view/admin/index.php
@@ -9,34 +9,18 @@
     <table class="table table-bordered">
         <thead class="thead-dark">
             <tr>
-                <th>Id</th>
-                <th>Avatar</th>
-                <th>Username</th>
-                <th>User's email</th>
-                <th>Activated ?</th>
-                <th>Link to user's profile</th>
-                <th>suspension Time in days</th>
-                <th>Soft delete</th>
-                <th>Submit</th>
+                <th>Email</th>
+                <th>Activado</th>
+                <th>Desactivar</th>
+                <th>Guardar</th>
             </tr>
         </thead>
         <tbody>
         <?php foreach ($this->users as $user) { ?>
             <tr>
-                <td><?= $user->user_id; ?></td>
-                <td class="avatar">
-                <?php if (isset($user->user_avatar_link)) { ?>
-                    <img src="<?= $user->user_avatar_link; ?>"/>
-                <?php } ?>
-                </td>
-                <td><?= $user->user_name; ?></td>
                 <td><?= $user->user_email; ?></td>
-                <td><?= ($user->user_active == 0 ? 'No' : 'Yes'); ?></td>
-                <td>
-                    <a href="<?= Config::get('URL') . 'profile/showProfile/' . $user->user_id; ?>">Profile</a>
-                </td>
+                <td><?= ($user->user_active == 0 ? 'No' : 'Si'); ?></td>
                 <form action="<?= config::get("URL"); ?>admin/actionAccountSettings" method="post">
-                    <td><input class="form-control" type="number" name="suspension" /></td>
                     <td><input type="checkbox" name="softDelete" <?php if ($user->user_deleted) { ?> checked <?php } ?> /></td>
                     <td>
                         <input type="hidden" name="user_id" value="<?= $user->user_id; ?>" />
