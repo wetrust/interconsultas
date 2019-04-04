28a0fd36c7f47613107bf616c3b8a49b1696f316
Cristopher Castro <cristophernic@live.cl>
2019-04-04 18:27:58 -0300

    datos

--- application/model/UserModel.php
+++ application/model/UserModel.php
@@ -7,7 +7,7 @@ class UserModel
     {
         $database = DatabaseFactory::getFactory()->getConnection();
 
-        $sql = "SELECT user_id, user_name, user_email, user_active, user_has_avatar, user_deleted FROM users";
+        $sql = "SELECT user_id, user_name, user_email, user_almacenamiento, user_active, user_has_avatar, user_deleted FROM users";
         $query = $database->prepare($sql);
         $query->execute();
 
