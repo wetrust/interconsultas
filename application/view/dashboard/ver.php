dfd1bbb80402484b5ffd31014889afc15a25e5ba
Cristopher Castro <cristophernic@live.cl>
2019-04-11 18:45:15 -0400

    si

--- application/view/dashboard/ver.php
+++ application/view/dashboard/ver.php
@@ -1,12 +1,10 @@
 <div class="container">
-    <h1 class="text-center">Ver interconsulta</h1>
+    <h1 class="text-center my-3">Ver interconsulta</h1>
     <?php $this->renderFeedbackMessages(); ?>
     <?php if ($this->solicitud) { ?>
-    <form method="post" action="<?php echo Config::get('URL'); ?>dashboard/editSave">
     <div class="card mt-1">
         <div class="card-body">
             <h4>Datos de la interconsulta</h4>
-            <input type="hidden" name="solicitud_id" value="<?php echo htmlentities($this->solicitud->solicitud_id); ?>" />
             <div class="row">
                 <div class="col form-group">
                     <label>Nombre del paciente:</label>
@@ -22,44 +20,42 @@
                 </div>
             </div>
             <div class="row">
-                    <div class="col form-group">
-                        <label>Ege conocida precozmente</label>
-                    </div>
-                    <div class="col form-group">
-                        <div class="form-check">
-                            <input type="radio" disabled value="0" <?php $check = ($this->solicitud->solicitud_eg == 0 ? "checked" : ""); echo $check; ?> >
-                            <label>No</label>
-                        </div>
-                        <div class="form-check">
-                            <input type="radio" disabled value="1" <?php $check = ($this->solicitud->solicitud_eg == 1 ? "checked" : ""); echo $check; ?> >
-                            <label>Si</label>
-                        </div>
-                    </div>
-                    <div class="col form-group">
-                        <label>Ecografía previa de crecimiento</label>
-                    </div>
-                    <div class="col form-group">
-                        <div class="form-check">
-                            <input type="radio" disabled value="0" <?php $check = ($this->solicitud->solicitud_eco == 0 ? "checked" : ""); echo $check; ?> >
-                            <label>No</label>
-                        </div>
-                        <div class="form-check">
-                            <input type="radio" disabled value="1" <?php $check = ($this->solicitud->solicitud_eco == 1 ? "checked" : ""); echo $check; ?> >
-                            <label>Si</label>
-                        </div>
-                    </div>
-                </div>
-            <div class="row">
                 <div class="col form-group">
-                    <label>FUM operacional</label>
-                    <input type="date" class="form-control" disabled value="<?php echo htmlentities($this->solicitud->solicitud_fum); ?>">
+                    <label>Ege conocida precozmente</label>
                 </div>
                 <div class="col form-group">
-                    <label>Edad Gestacional</label>
-                    <input type="text" class="form-control" disabled value="<?php echo htmlentities($this->solicitud->solicitud_egestacional); ?>">
+                    <div class="form-check">
+                        <input type="radio" disabled value="0" <?php $check = ($this->solicitud->solicitud_eg == 0 ? "checked" : ""); echo $check; ?> >
+                        <label>No</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" disabled value="1" <?php $check = ($this->solicitud->solicitud_eg == 1 ? "checked" : ""); echo $check; ?> >
+                        <label>Si</label>
+                    </div>
+                </div>
+                <div class="col form-group">
+                    <label>Ecografía previa de crecimiento</label>
+                </div>
+                <div class="col form-group">
+                    <div class="form-check">
+                        <input type="radio" disabled value="0" <?php $check = ($this->solicitud->solicitud_eco == 0 ? "checked" : ""); echo $check; ?> >
+                        <label>No</label>
+                    </div>
+                    <div class="form-check">
+                        <input type="radio" disabled value="1" <?php $check = ($this->solicitud->solicitud_eco == 1 ? "checked" : ""); echo $check; ?> >
+                        <label>Si</label>
+                    </div>
                 </div>
             </div>
             <div class="row">
+                <div class="col-3 form-group">
+                    <label>FUM operacional</label>
+                    <input type="date" class="form-control" disabled value="<?php echo htmlentities($this->solicitud->solicitud_fum); ?>">
+                </div>
+                <div class="col-3 form-group">
+                    <label>Edad Gestacional</label>
+                    <input type="text" class="form-control" disabled value="<?php echo htmlentities($this->solicitud->solicitud_egestacional); ?>">
+                </div>
                 <div class="col form-group">
                     <label>Diagnóstico de referencia</label>
                     <input type="text" class="form-control" disabled value="<?php echo htmlentities($this->solicitud->solicitud_diagnostico); ?>">
@@ -130,111 +126,110 @@
     </div>
     <div class="card mt-3">
         <div class="card-body">
-            <h4 class="text-center">Respuesta de profesional contrarreferente a solicitud de exámen ecográfico</h4>
+            <h4 class="my-3">Respuesta de profesional contrarreferente a solicitud de exámen ecográfico</h4>
             <div class="row">
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.fecha">Fecha evaluación de interconsulta</label>
-                        <input disabled type="date" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->fecha); ?>">
-                    </div>
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.eg">Edad gestacional actual</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->eg); ?>">
-                    </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.fecha">Fecha evaluación de interconsulta</label>
+                    <input disabled type="date" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->fecha); ?>">
                 </div>
-                <div class="row">
-                    <div class="col form-group">
-                        <label>Feto en presentación</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->presentacion); ?>">
-                    </div>
-                    <div class="col form-group">
-                        <label>Dorso fetal</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->dorso); ?>">
-                    </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.eg">Edad gestacional actual</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->eg); ?>">
                 </div>
-                <div class="row" id="interconsulta.respuesta.liquido.div">
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.liquido">Líquido amniótico</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->liquido); ?>">
-                    </div>
+            </div>
+            <div class="row">
+                <div class="col form-group">
+                    <label>Feto en presentación</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->presentacion); ?>">
                 </div>
-                <div class="row" id="interconsulta.respuesta.pfe.div">
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.pfe"><strong>A.- Biometría ecográfica:</strong><br>Peso fetal estimado</label>
-                        <input disabled type="number" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->pfe); ?>">
-                    </div>
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.pfe.percentil">&nbsp;<br>Percentil</label>
-                        <input disabled type="text" class="form-control" disabled="" value="<?php echo htmlentities($this->solicitud_resultado->pfe_percentil); ?>">
-                    </div>
+                <div class="col form-group">
+                    <label>Dorso fetal</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->dorso); ?>">
                 </div>
-                <div class="row" id="interconsulta.respuesta.uterinas.div">
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.uterinas"><strong>B.- Flujometría Doppler</strong><br>IP. Promedio uterinas</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->uterinas); ?>">
-                    </div>
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.uterinas.percentil">&nbsp;<br>Percentil</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->uterinas_percentil); ?>">
-                    </div>
+            </div>
+            <div class="row" id="interconsulta.respuesta.liquido.div">
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.liquido">Líquido amniótico</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->liquido); ?>">
                 </div>
-                <div class="row" id="interconsulta.respuesta.umbilical.div">
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.umbilical">IP. Arteria umbilical</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->umbilical); ?>">
-                    </div>
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.umbilical.percentil">Percentil</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->umbilical_percentil); ?>">
-                    </div>
+            </div>
+            <div class="row" id="interconsulta.respuesta.pfe.div">
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.pfe"><strong>A.- Biometría ecográfica:</strong><br>Peso fetal estimado</label>
+                    <input disabled type="number" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->pfe); ?>">
                 </div>
-                <div class="row" id="interconsulta.respuesta.cm.div">
-                    <div class="col form-group">
-                        <label for="interconsulta.respuesta.cm">IP. Cerebral media</label>
-                        <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->cm); ?>">
-                    </div>
-                        <div class="col form-group">
-                            <label for="interconsulta.respuesta.cm.percentil">Percentil</label>
-                            <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->cm_percentil); ?>">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col form-group">
-                            <label for="interconsulta.respuesta.cmau">Cuociente CM / AU</label>
-                            <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->cmau); ?>">
-                        </div>
-                        <div class="col form-group">
-                            <label for="interconsulta.respuesta.cmau.percentil">Percentil</label>
-                            <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->cmau_percentil); ?>">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-12">
-                            <p>Hipótesis diagnóstica</p>
-                        </div>
-                        <div class="col form-group">
-                            <label for="interconsulta.respuesta.hipotesis">Crecimiento fetal</label>
-                            <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->hipotesis); ?>">
-                        </div>
-                        <div class="col form-group">
-                            <label for="interconsulta.respuesta.hipotesis">Flujometría Doppler</label>
-                            <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->doppler); ?>">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col form-group">
-                            <label for="interconsulta.respuesta.comentariosexamen">Comentarios de exámen</label>
-                            <?php echo $this->solicitud_resultado->comentariosexamen; ?>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col form-group">
-                            <label for="interconsulta.respuesta.ecografista">Ecografista</label>
-                            <input disabled type="text" class="form-control" id="respuesta.ecografista" value="<?php echo htmlentities($this->solicitud_resultado->ecografista); ?>">
-                        </div>
-                    </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.pfe.percentil">&nbsp;<br>Percentil</label>
+                    <input disabled type="text" class="form-control" disabled="" value="<?php echo htmlentities($this->solicitud_resultado->pfe_percentil); ?>">
+                </div>
+            </div>
+            <div class="row" id="interconsulta.respuesta.uterinas.div">
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.uterinas"><strong>B.- Flujometría Doppler</strong><br>IP. Promedio uterinas</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->uterinas); ?>">
+                </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.uterinas.percentil">&nbsp;<br>Percentil</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->uterinas_percentil); ?>">
+                </div>
+            </div>
+            <div class="row" id="interconsulta.respuesta.umbilical.div">
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.umbilical">IP. Arteria umbilical</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->umbilical); ?>">
+                </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.umbilical.percentil">Percentil</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->umbilical_percentil); ?>">
+                </div>
+            </div>
+            <div class="row" id="interconsulta.respuesta.cm.div">
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.cm">IP. Cerebral media</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->cm); ?>">
+                </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.cm.percentil">Percentil</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->cm_percentil); ?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.cmau">Cuociente CM / AU</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->cmau); ?>">
+                </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.cmau.percentil">Percentil</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->cmau_percentil); ?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p>Hipótesis diagnóstica</p>
+                </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.hipotesis">Crecimiento fetal</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->hipotesis); ?>">
+                </div>
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.hipotesis">Flujometría Doppler</label>
+                    <input disabled type="text" class="form-control" value="<?php echo htmlentities($this->solicitud_resultado->doppler); ?>">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.comentariosexamen">Comentarios de exámen</label>
+                    <?php echo $this->solicitud_resultado->comentariosexamen; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col form-group">
+                    <label for="interconsulta.respuesta.ecografista">Ecografista</label>
+                    <input disabled type="text" class="form-control" id="respuesta.ecografista" value="<?php echo htmlentities($this->solicitud_resultado->ecografista); ?>">
+                </div>
+            </div>
         </div>
     </div>
-    </form>
     <?php } else { ?>
         <div class="alert alert-danger" role="alert">Esta interconsulta no existe.</div>
     <?php } ?>
