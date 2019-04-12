61c180976e7f234d7639394c28a82dd0184d715c
Cristopher Castro <cristophernic@live.cl>
2019-04-12 00:13:26 -0400

    .

--- application/view/pdf/finalinforme/index.php
+++ application/view/pdf/finalinforme/index.php
@@ -133,7 +133,7 @@
     $html = '<h2 style="border-top:1px solid #000;text-align: left;">&nbsp;</h2>';
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
 
-    $html = '<p>Informe generado desde software crecimientofetal.cl, el objetivo de este es favorecer análisis preeliminar de datos obtenidos en el examen ecográfico, la interpretación clínica de los resultados es responsabilidad exclusiva de quien procesa esta información.</p>';
+    $html = '<p>Informe generado desde software crecimientofetal.cl, el objetivo de este, es favorecer análisis preeliminar de datos obtenidos en el examen ecográfico, la interpretación clínica de los resultados es responsabilidad exclusiva de quien procesa esta información.</p>';
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
 
     $tmp = Config::get('PATH_AVATARS');
