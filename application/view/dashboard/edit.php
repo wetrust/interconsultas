c08b60bc423ee01ce428637d90f92aadd71f6f15
Cristopher Castro <cristophernic@live.cl>
2019-04-19 22:12:39 -0400

    prueba ccca

--- application/view/dashboard/edit.php
+++ application/view/dashboard/edit.php
@@ -580,7 +580,22 @@
                     $("input[name='respuesta_pfe_pct_segundo']").val(pctpfeAdvanced(eg,pfe));
                     $("input[name='respuesta_pfe_pct_ver_segundo']").val(pctpfeAdvanced(eg,pfe));
                 }
+            });
 
+            $("input[name='respuesta_ccca']").on("change", function(){
+                
+                var eg = $("#interconsulta\\.respuesta\\.eg").val();
+                var ccca = $("input[name='respuesta_ccca']").val();
+
+                eg = String(eg);
+                eg = eg.replace("semanas", "");
+
+                if (eg.length > 0){
+
+                    eg =  parseFloat(eg).toFixed();
+                    $("input[name='respuesta_ccca_pct']").val(pctcccaAdvanced(eg,ccca));
+                    $("input[name='respuesta_ccca_pct_ver']").val(pctcccaAdvanced(eg,ccca));
+                }
             });
 
             $('#interconsulta\\.respuesta\\.fecha').on('change', function () {
@@ -1000,12 +1015,14 @@
             var CA = 0;
 
             if (parseInt($("input[name='respuesta_cc']").val()) < 0){
-                $("input[name='respuesta_pfe']").val("0");
+                $("input[name='respuesta_ccca_ver']").val(0);
+                $("input[name='respuesta_ccca']").val(0).trigger("change");
                 return;
             }
 
             if (parseInt($("input[name='respuesta_ca']").val()) < 0){
-                $("input[name='respuesta_pfe']").val("0");
+                $("input[name='respuesta_ccca_ver']").val(0);
+                $("input[name='respuesta_ccca']").val(0).trigger("change");
                 return;
             }
 
@@ -1018,11 +1035,11 @@
 
             if (isNaN(ccca) != true) {
                 $("input[name='respuesta_ccca_ver']").val(ccca.toFixed(2));
-                $("input[name='respuesta_ccca']").val(ccca.toFixed(2));
+                $("input[name='respuesta_ccca']").val(ccca.toFixed(2)).trigger("change");
             }
             else{
                 $("input[name='respuesta_ccca_ver']").val(0);
-                $("input[name='respuesta_ccca']").val(0);
+                $("input[name='respuesta_ccca']").val(0).trigger("change");
             }
         }
 
@@ -1087,6 +1104,51 @@
                 $("input[name='respuesta_lcn_eg']").val(0);
             }
         };
+
+        function pctcccaAdvanced(eg, ccca) {
+            var pct3 = [];
+            var pct97 = [];
+
+            pct3[0] = 1.1; pct3[1] = 1.09; pct3[2] = 1.08; pct3[3] = 1.07;
+            pct3[4] = 1.06; pct3[5] = 1.06; pct3[6] = 1.05; pct3[7] = 1.04;
+            pct3[8] = 1.03; pct3[9] = 1.02; pct3[10] = 1.01; pct3[11] = 1;
+            pct3[12] = 1; pct3[13] = 0.99; pct3[14] = 0.98; pct3[15] = 0.97;
+            pct3[16] = 0.96; pct3[17] = 0.95; pct3[18] = 0.95; pct3[19] = 0.94;
+            pct3[20] = 0.93; pct3[21] = 0.92; pct3[22] = 0.91; pct3[23] = 0.9;
+            pct3[24] = 0.89; pct3[25] = 0.89;
+
+            pct97[0] = 1.29; pct97[1] = 1.28; pct97[2] = 1.27; pct97[3] = 1.26;
+            pct97[4] = 1.25; pct97[5] = 1.24; pct97[6] = 1.24; pct97[7] = 1.23;
+            pct97[8] = 1.22; pct97[9] = 1.21; pct97[10] = 1.2; pct97[11] = 1.19;
+            pct97[12] = 1.18; pct97[13] = 1.18; pct97[14] = 1.17; pct97[15] = 1.17;
+            pct97[16] = 1.16; pct97[17] = 1.15; pct97[18] = 1.14; pct97[19] = 1.13;
+            pct97[20] = 1.12; pct97[21] = 1.11; pct97[22] = 1.1; pct97[23] = 1.09;
+            pct97[24] = 1.08; pct97[25] = 1.08;
+
+            if (eg < 15) {
+                return 0;
+            } 
+            else if (eg > 40)
+            {
+                return 0;
+            } 
+            else {
+                eg = eg - 15;
+                eg = parseInt(eg);
+                var uno = pct97[eg] - pct3[eg];
+                var dos = ccca - pct3[eg];
+                var resultado = parseInt(95 / (uno) * (dos) + 3);
+                if (resultado > 99) {
+                    return '> 99';
+                } 
+                else if (resultado < 1) {
+                    return '< 1';
+                } 
+                else {
+                    return resultado;
+                }
+            }
+        }
     </script>
     </form>
     <?php } else { ?>
