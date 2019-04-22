e53b9de1018c51e5cd292daf8ae7a5874af39101
Cristopher Castro <cristophernic@live.cl>
2019-04-22 15:36:31 -0400

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -92,7 +92,7 @@
                                     <th>Rut</th>
                                     <th>Fecha</th>
                                     <th>Diagnóstico</th>
-                                    <th>Accion</th>
+                                    <th colspan="2">Accion</th>
                                     <th>Eliminar</th>
                                 </tr>
                             </thead>
@@ -105,13 +105,13 @@
                                     <td><?= htmlentities($value->solicitud_fecha); ?></td>
                                     <td><?= htmlentities($value->solicitud_diagnostico); ?></td>
                                     <?php if ($value->tipo == 1){ ?>
-                                    <td><a class="btn btn-primary" href="<?= Config::get('URL') . 'pdf/informe_primertrimestre/' . $value->solicitud_id; ?>">Ver</a> / <a href="#" class="btn btn-primary linkemail" data-informe="<?= $value->tipo; ?>" data-solicitud="<?= $value->solicitud_id; ?>">Reenviar</a></td>
+                                    <td><a class="btn btn-primary" href="<?= Config::get('URL') . 'pdf/informe_primertrimestre/' . $value->solicitud_id; ?>">Ver</a></td><td><a href="#" class="btn btn-primary linkemail" data-informe="<?= $value->tipo; ?>" data-solicitud="<?= $value->solicitud_id; ?>">Reenviar</a></td>
                                     <?php } else if ($value->tipo == 0){ ?>
-                                    <td><a class="btn btn-primary" href="<?= Config::get('URL') . 'pdf/informe_dopplercrecimiento/' . $value->solicitud_id; ?>">Ver</a> / <a href="#" class="btn btn-primary linkemail" data-informe="<?= $value->tipo; ?>" data-solicitud="<?= $value->solicitud_id; ?>">Reenviar</a></td>
+                                    <td><a class="btn btn-primary" href="<?= Config::get('URL') . 'pdf/informe_dopplercrecimiento/' . $value->solicitud_id; ?>">Ver</a></td><td><a href="#" class="btn btn-primary linkemail" data-informe="<?= $value->tipo; ?>" data-solicitud="<?= $value->solicitud_id; ?>">Reenviar</a></td>
                                     <?php } else if ($value->tipo == 2){ ?>
-                                    <td><a class="btn btn-primary" href="<?= Config::get('URL') . 'pdf/informe_segundotrimestre/' . $value->solicitud_id; ?>">Ver</a> / <a href="#" class="btn btn-primary linkemail" data-informe="<?= $value->tipo; ?>" data-solicitud="<?= $value->solicitud_id; ?>">Reenviar</a></td>
+                                    <td><a class="btn btn-primary" href="<?= Config::get('URL') . 'pdf/informe_segundotrimestre/' . $value->solicitud_id; ?>">Ver</a></td><td><a href="#" class="btn btn-primary linkemail" data-informe="<?= $value->tipo; ?>" data-solicitud="<?= $value->solicitud_id; ?>">Reenviar</a></td>
                                     <?php } else if ($value->tipo == 3){ ?>
-                                    <td><a class="btn btn-primary" href="<?= Config::get('URL') . 'pdf/informe_ginecologico/' . $value->solicitud_id; ?>">Ver</a> / <a href="#" class="btn btn-primary linkemail" data-informe="<?= $value->tipo; ?>" data-solicitud="<?= $value->solicitud_id; ?>">Reenviar</a></td>
+                                    <td><a class="btn btn-primary" href="<?= Config::get('URL') . 'pdf/informe_ginecologico/' . $value->solicitud_id; ?>">Ver</a></td><td><a href="#" class="btn btn-primary linkemail" data-informe="<?= $value->tipo; ?>" data-solicitud="<?= $value->solicitud_id; ?>">Reenviar</a></td>
                                     <?php } ?>
                                     <td><a class="btn btn-danger" href="<?= Config::get('URL') . 'dashboard/delete/' . $value->solicitud_id; ?>">Eliminar</a></td>
                                 </tr>
