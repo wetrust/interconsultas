70e4b04ebaf545365da5b256fe413f502e4030ac
Cristopher Castro <cristophernic@live.cl>
2019-04-29 18:22:21 -0400

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -302,7 +302,7 @@
                             tabla += '<td><a class="btn btn-primary mr-3" href="' + _URL + 'pdf/informe_ginecologico/' + value.solicitud_id + '">Ver</a><a href="#" class="btn btn-primary linkemail" data-informe='+ value.tipo +' data-solicitud=' + value.solicitud_id + '>Reenviar</a></td>';
                         }
 
-                        tabla += '<td><a class="btn btn-danger" href="' + _URL + 'dashboard/delete/' + value.solicitud_id; +'">Eliminar</a></tr>';
+                        tabla += '<td><a class="btn btn-danger" href="' + _URL + 'dashboard/delete/' + value.solicitud_id; +'">Eliminar</a></td></tr>';
                     });
 
                     tabla += '</tbody></table>';
