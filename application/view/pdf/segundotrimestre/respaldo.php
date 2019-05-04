9a7ba3a21125320ee59b1a6cec4ed545feb40373
Cristopher Castro <cristophernic@live.cl>
2019-05-04 17:11:51 -0400

    .

---
+++ application/view/pdf/segundotrimestre/respaldo.php
@@ -0,0 +1,118 @@
+<?php
+    // set document information
+    $this->pdf->SetCreator(PDF_CREATOR);
+    $this->pdf->SetAuthor('WT');
+    $this->pdf->SetTitle('Evaluación ecográfica del crecimiento fetal');
+    $this->pdf->SetSubject('TCPDF Tutorial');
+    $this->pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+
+    // set default header data
+    //$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 009', PDF_HEADER_STRING);
+
+    // set header and footer fonts
+    //$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
+    //$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
+    // set default monospaced font
+    $this->pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+
+    // set margins
+    $this->pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+    $this->pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
+    $this->pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
+    $this->pdf->setImageScale(PDF_IMAGE_SCALE_RATIO);
+
+    // -------------------------------------------------------------------
+
+    // add a page
+    $this->pdf->AddPage('P', 'LETTER');
+
+    // set JPEG quality
+    $this->pdf->setJPEGQuality(90);
+
+    $this->pdf->SetFont('Helvetica', '', 8);
+    $html = '<h3 style="background-color:#0275d8;color:white;text-align:center;">RESUMEN PROTOCOLO DE REFERENCIA Y CONTRARREFERENCIA PARA ECOGRAFÍA OBSTÉTRICA</h3>';
+    $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'C', true);
+    $this->pdf->Ln(2);
+    $html = '<h4 style="border-bottom:1px solid #000;color:#0275d8;">A- Formulario referencia para evaluación ecográfica del crecimiento fetal</h4>';
+    $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
+    $this->pdf->Ln(2);
+    $html = '<table><tbody><tr><td>Nombre del paciente: Paciente 1ra.</td><td>RUT (DNI): 11.111.111-1</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(1);
+    $html = '<table><tbody><tr><td>Fecha de solicitud: 12-04-2019</td><td>FUM Operacional: 28-09-2018</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(1);
+    $html = '<table><tbody><tr><td>Diagnóstico de referencia:  Evaluación del crecimiento</td><td>Edad Gestacional solicitud: 28.0 semanas</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(1);
+    $html = '<table><tbody><tr><td>Ciudad procedencia: Loncoche</td><td>Lugar de control: Consultorio La Paz</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(2);
+    $html = '<table><tbody><tr><td style="background-color:#eceeef;">Profesional referente</td><td>Matrona</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, '', true);
+    $html = '<table><tbody><tr><td></td><td>Nombre: Maria Angelica A.</td></tr><tr><td></td><td>Email: rudecindo.lagos@ufrontera.cl</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(2);
+    $html = '<table><tbody><tr><td style="background-color:#eceeef;">Ecografista de contrarreferencia</td><td>Nombre: Dr. R. Lagos Tco. </td></tr><tr><td></td><td>Email: examenecografico@gmail.com</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, '', true);
+    $this->pdf->Ln(8);
+
+    $html = '<h4 style="border-bottom:1px solid #000;color:#0275d8;">B- Contrarreferencia inicial desde unidad de ultrasonografía gineco obstétrica</h4>';
+    $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
+    $this->pdf->Ln(2);
+    $html = '<table><tbody><tr><td>Evaluación de solicitud ecográfica: 12-04-2019</td><td>Comentario: ENVÍELA A EXÁMEN EL LUNES PRÓXIMO</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(8);
+
+    $html = '<h4 style="border-bottom:1px solid #000;color:#0275d8;">C- Respuesta de profesional contrarreferente a solicitud de exámen ecográfico</h4>';
+    $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
+    $this->pdf->Ln(2);
+    $html = '<table><tbody><tr><td>Edad Gestacional al exámen: 20.1 semanas</td><td>Feto en presentación: cefálica</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(1);
+    $html = '<table><tbody><tr><td>Dorso Fetal: anterior</td><td>Líquido amniótico: Normal</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(1);
+    $html = '<table><tbody><tr><td>Anatomía fetal: , de aspecto general normal.</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(4);
+    $html = '<table><tbody><tr><td><strong><em>Biometría ecográfica</em></strong></td><td>Peso fetal estimado:</td><td> 1000 gr.</td><td>Percentil: 9</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(2);
+    $html = '<table><tbody><tr><td><strong><em>Flujometría Doppler</em></strong></td><td>IP Promedio uterinas:</td><td> 0.90</td><td>Percentil: 55</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $html = '<table><tbody><tr><td></td><td>IP Arteria umbilical (UMB):</td><td> 1.20</td><td>Percentil: 69</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $html = '<table><tbody><tr><td></td><td>IP Cerebral media (ACM):</td><td> 1.87</td><td>Percentil: 69</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $html = '<table><tbody><tr><td></td><td>Cuociente Cm / Au (CCP):</td><td> 1.56</td><td>Percentil: 70</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(2);
+
+    $html = '<table><tbody><tr><td style="width:162px"><strong><em>Hipótesis diagnóstica *</em></strong></td><td style="width:450px">Crecimiento fetal: Disminuido < p 10</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(1);
+    $html = '<table><tbody><tr><td style="width:162px"></td><td style="width:450px">Flujometría Doppler: Materno Normal + Fetal Normal</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(1);
+    $html = '<table><tbody><tr><td style="width:162px"></td><td style="width:450px">Líquido amniótico: Normal</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(4);
+    $html = '<table><tbody><tr><td style="width:162px"><strong><em>Comentarios y observaciones</em></strong></td><td style="width:450px">FETO PEQUEÑO CRECIENDO EN PERCENTOL 9, LA FLUJOMETRÍA DOPPLER MATERNO/FETAL ES NORMAL<br>SE SUGUIERE REEVALUAR EN 7 DIAS EN UNIDAD DE ARO DE HOSPITAL REGIONAL TEMUCO O PITRUFQUEN.</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(8);
+
+    $html = '<table><tbody><tr><td style="width:450px"></td><td>Ecografista: Dr. Rudecindo Lagos</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(4);
+    $html = '<table style="border-top:1px solid #000;border-bottom:1px solid #000;"><tbody><tr><td><p>Fecha de exámen: 14-04-2019</p></td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
+    $this->pdf->Ln(4);
+    $html = '<p>Informe generado desde software crecimientofetal.cl, el objetivo de este, es favorecer análisis preeliminar de datos obtenidos en el examen ecográfico, la interpretación clínica de los resultados es responsabilidad exclusiva del profesional referente, quien procesa esta información.</p>';
+    $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
+    $this->pdf->Output('example.pdf', 'I');
+    //Close and output PDF document
+    //$base64 = chunk_split(base64_encode($this->pdf->Output('Informe.pdf', 'S')));
+
+    //echo $base64;
