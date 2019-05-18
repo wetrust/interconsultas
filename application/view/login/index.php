07f3a3c649819de35028156de19ab3444bb4d71f
Cristopher Castro <cristophernic@live.cl>
2019-05-18 01:46:13 -0400

    .

--- application/view/login/index.php
+++ application/view/login/index.php
@@ -42,11 +42,15 @@
         </div>
         <div class="col-8 mt-2">
             <div class="mx-3 px-5">
-                <p class="mt-2 text-justify">El objetivo de esta herramienta informática es favorecer el sistema de referencia y contrareferencia para el exámen ecográfico obstétrico entre los distintos niveles de atención en salud materna.
-                <img class="mx-auto d-block" src="imagenes/ecografo.png" alt="Ecógrafo">
-                <br>El profesional contrareferente a quien le fue solicitada la interconsulta deberá estar previamente registrado en la plataforma.</p>
-                <p class="text-primary text-justify">Si ud. desea probar la plataforma de interconsultas, puede usar el usuario invitado (Usuario: prueba@prueba.cl; Contraseña: 123abc).</p>
-                <p class="text-primary text-justify">Si es de su interés adquirir este servicio es necesario contratar un servidor cloud con proveedores como "Google Cloud Computing" o "Amazon AWZ".</p>
+            <ul class="mt-2">
+                <li class="text-justify">El objetivo de esta herramienta informática es favorecer el sistema de referencia y contrareferencia para el exámen ecográfico obstétrico entre los distintos niveles de atención en salud materna.</li>
+            </ul>
+            <img class="mx-auto d-block" src="imagenes/ecografo.png" alt="Ecógrafo">
+            <ul>
+                <li class="text-justify">El profesional contrareferente a quien le fue solicitada la interconsulta deberá estar previamente registrado en la plataforma.</li>
+                <li class="text-justify text-primary">Si ud. desea probar la plataforma de interconsultas, puede usar el usuario invitado (Usuario: prueba@prueba.cl; Contraseña: 123abc).</li>
+                <li class="text-justify">Si es de su interés adquirir este servicio es necesario contratar un servidor cloud con proveedores como "Google Cloud Computing" o "Amazon AWZ".</li>
+            </ul>
             </div>
         </div>
     </div>
