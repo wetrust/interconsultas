b8563b8af647d0b16862f17a40b27a037f496be0
Cristopher Castro <cristophernic@live.cl>
2019-05-19 17:26:36 -0400

    .

--- application/view/contacto/index.php
+++ application/view/contacto/index.php
@@ -2,19 +2,26 @@
     <h5>Formulario de contacto</h5>
     <p>Use este formulario para solicitar una suscripción para este sistema o para contactarnos</p>
     <form method="post" action="<?php echo Config::get('URL');?>contacto/enviar">
-        <div class="form-group">
-            <label>Su nombre:</label>
-            <input type="text" class="form-control" name="note_text" autocomplete="off">
+        <div class="row">
+            <div class="form-group col">
+                <label>Nombre:</label>
+                <input type="text" class="form-control" name="nombre_text" autocomplete="off">
+            </div>
+            <div class="form-group col">
+                <label>Dirección postal:</label>
+                <input type="text" class="form-control" name="nombre_text" autocomplete="off">
+            </div>
         </div>
         <div class="form-group">
-            <label>Su correo electronico:</label>
-            <input type="text" class="form-control" name="note_text" autocomplete="off">
+            <label>Correo electronico:</label>
+            <input type="email" class="form-control" name="email_text" autocomplete="off">
         </div>
         <div class="form-group">
             <label>Mensaje:</label>
-            <textarea type="text" class="form-control" name="note_text" autocomplete="off"></textarea>
+            <textarea class="form-control" name="mensaje_text" autocomplete="off"></textarea>
         </div>
         <input type="hidden" name="csrf_token" value="<?= Csrf::makeToken(); ?>" />
         <button type="submit" class="btn btn-primary">Enviar mensajes</button>
+        <p>Al adquirir este servicio y guardar su información, es necesario contratar un servidor Cloud ejemplo: "Google Cloud Platform",  "Amazon AWZ"</p>
     </form>
 </div>
