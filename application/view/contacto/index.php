d7fadd87a9a11b10bc613a0cf3f4524db3180c00
Cristopher Castro <cristophernic@live.cl>
2019-05-19 17:32:22 -0400

    .

--- application/view/contacto/index.php
+++ application/view/contacto/index.php
@@ -1,33 +1,37 @@
 <div class="container">
-    <h5>Formulario de contacto</h5>
-    <p>Use este formulario para solicitar una suscripción para este sistema o para contactarnos</p>
-    <form method="post" action="<?php echo Config::get('URL');?>contacto/enviar">
-        <div class="row">
-            <div class="form-group col">
-                <label>Nombre:</label>
-                <input type="text" class="form-control" name="nombre_text" autocomplete="off">
-            </div>
-            <div class="form-group col">
-                <label>Dirección postal:</label>
-                <input type="text" class="form-control" name="direccion_text" autocomplete="off">
-            </div>
+    <div class="card">
+        <div class="card-body">
+            <h5>Formulario de contacto</h5>
+            <p>Use este formulario para solicitar una suscripción para este sistema o para contactarnos</p>
+            <form method="post" action="<?php echo Config::get('URL');?>contacto/enviar">
+                <div class="row">
+                    <div class="form-group col">
+                        <label>Nombre:</label>
+                        <input type="text" class="form-control" name="nombre_text" autocomplete="off">
+                    </div>
+                    <div class="form-group col">
+                        <label>Dirección postal:</label>
+                        <input type="text" class="form-control" name="direccion_text" autocomplete="off">
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col">
+                        <label>Correo electronico:</label>
+                        <input type="email" class="form-control" name="email_text" autocomplete="off">
+                    </div>
+                    <div class="form-group col">
+                        <label>Teléfono:</label>
+                        <input type="email" class="form-control" name="telefono_text" autocomplete="off">
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Mensaje:</label>
+                    <textarea class="form-control" name="mensaje_text" autocomplete="off"></textarea>
+                </div>
+                <input type="hidden" name="csrf_token" value="<?= Csrf::makeToken(); ?>" />
+                <p>Para acceder a esta plataforma y guardar datos de exámenes ecográficos, es necesario contratar un servidor Cloud, ejemplo: "Google Cloud Platform",  "Amazon AWZ"</p>
+                <button type="submit" class="btn btn-primary">Enviar mensajes</button>
+            </form>
         </div>
-        <div class="row">
-            <div class="form-group col">
-                <label>Correo electronico:</label>
-                <input type="email" class="form-control" name="email_text" autocomplete="off">
-            </div>
-            <div class="form-group col">
-                <label>Teléfono:</label>
-                <input type="email" class="form-control" name="telefono_text" autocomplete="off">
-            </div>
-        </div>
-        <div class="form-group">
-            <label>Mensaje:</label>
-            <textarea class="form-control" name="mensaje_text" autocomplete="off"></textarea>
-        </div>
-        <input type="hidden" name="csrf_token" value="<?= Csrf::makeToken(); ?>" />
-        <p>Para acceder a esta plataforma y guardar datos de exámenes ecográficos, es necesario contratar un servidor Cloud, ejemplo: "Google Cloud Platform",  "Amazon AWZ"</p>
-        <button type="submit" class="btn btn-primary">Enviar mensajes</button>
-    </form>
+    </div>
 </div>
