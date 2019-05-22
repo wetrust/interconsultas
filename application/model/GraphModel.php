b8fb8e7ff3ab8688bed8b7c05c623358b6759825
Cristopher Castro <cristophernic@live.cl>
2019-05-22 16:18:38 -0400

    .

--- application/model/GraphModel.php
+++ application/model/GraphModel.php
@@ -78,7 +78,7 @@ class GraphModel
             'graph_title'       => 'Cc / Ca',
             'show_grid_v'       => false,
             'semantic_classes'  => false,
-            'show_data_labels'  => false
+            'show_data_labels'  => false,
             'show_tooltips'     => false,
         );
           
