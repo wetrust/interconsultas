11cc7fabe7212e3fedab47a0b1232f55c3d7ea6c
Cristopher Castro <cristophernic@live.cl>
2019-05-22 21:11:51 -0400

    .

--- application/model/GraphModel.php
+++ application/model/GraphModel.php
@@ -1,6 +1,6 @@
 <?php
 
-class GraphModel
+class GraphicoModel
 {
     private static $factory;
     private $graph;
@@ -8,18 +8,52 @@ class GraphModel
     public static function getFactory()
     {
         if (!self::$factory) {
-            self::$factory = new GraphModel();
+            self::$factory = new GraphicoModel();
         }
         return self::$factory;
     }
 
     public static function getInstance(){
-        if (!self::$graph) {
-            self::$graph = new Goat1000\SVGGraph\SVGGraph(300, 200, self::settings());
+        if ($this->graph) {
+            $this->graph = new Goat1000\SVGGraph\SVGGraph(300, 200, self::settings());
         }
-        return self::$graph;
+        return $this->graph;
     }
+    public static function settings(){
+        $settings = array(
+            'back_colour'       => '#FFF',
+            'stroke_colour'     => '#000',
+            'back_stroke_width' => 0,
+            'back_stroke_colour'=> '#eee',
+            'axis_colour'       => '#333',
+            'axis_overlap'      => 2,
+            'axis_font'         => 'Georgia',
+            'axis_font_size'    => 10,
+            'grid_colour'       => '#666',
+            'label_colour'      => '#000',
+            'pad_right'         => 20,
+            'pad_left'          => 20,
+            'link_base'         => '/',
+            'link_target'       => '_top',
+            'fill_under'        => array(false, false),
+            'marker_size'       => array(1, 1, 5),
+            'marker_type'       => array('circle', 'circle', 'cross'),
+            'label_x'           => "Semanas",
+            'axis_min_h'        => '15',
+            'axis_min_v'        => '0.75',
+            'graph_title'       => 'Cc / Ca',
+            'show_grid_v'       => false,
+            'semantic_classes'  => false,
+            'show_data_labels'  => false,
+            'show_tooltips'     => false,
+        );
 
+        return $settings;
+    }
+}
+
+class GraphModel
+{
     public static function pesoFetal($EG, $PESO)
     {
         $settings = self::settings();
@@ -31,7 +65,7 @@ class GraphModel
         $punto = array($EG => $PESO);
         array_push($values,$punto);
 
-        $graph = self::getFactory()->getInstance();
+        $graph = GraphicoModel::getFactory()->getInstance();
         $graph->colours(self::colours());
         $graph->values($values);
         //$graph->settings($settings);
