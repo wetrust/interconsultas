aa9e8a510a85bb13b04ca089f8bbe1fed151c4fd
Cristopher Castro <cristophernic@live.cl>
2019-05-22 22:45:49 -0400

    .

--- application/model/GraphModel.php
+++ application/model/GraphModel.php
@@ -16,7 +16,7 @@ class GraphModel
         $punto = array($EG => $PESO);
         array_push($values,$punto);
 
-        $graph = new Goat1000\SVGGraph\SVGGraph(180, 180, $settings);
+        $graph = new Goat1000\SVGGraph\SVGGraph(200, 180, $settings);
         $graph->colours(self::colours());
         $graph->values($values);
         return $graph->fetch('MultiLineGraph');
@@ -33,7 +33,7 @@ class GraphModel
         $punto = array($EG => $valor);
         array_push($values,$punto);
 
-        $graph = new Goat1000\SVGGraph\SVGGraph(200, 150, $settings);
+        $graph = new Goat1000\SVGGraph\SVGGraph(200, 180, $settings);
         $graph->colours(self::colours());
         $graph->values($values);
         return $graph->fetch('MultiLineGraph');
