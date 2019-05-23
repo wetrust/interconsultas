b5d69e646b7f77b4100e5211de0d7b01bfa9a6ee
Cristopher Castro <cristophernic@live.cl>
2019-05-22 22:08:10 -0400

    .

--- application/view/pdf/finalinforme/index_grafico_ver.php
+++ application/view/pdf/finalinforme/index_grafico_ver.php
@@ -39,7 +39,7 @@
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'C', true);
     $this->pdf->Ln(2);
 
-    $this->pdf->ImageSVG('@' . $this->grafico_uno, $x=0, $y=0, $w='', $h=100, $link='', $align='', $palign='', $border=0, $fitonpage=false);
+    $this->pdf->ImageSVG('@' . $this->grafico_uno, $x=0, $y=10, $w='', $h=100, $link='', $align='', $palign='', $border=0, $fitonpage=false);
     $this->pdf->ImageSVG('@' . $this->grafico_uno, $x=50, $y=100, $w='', $h=100, $link='', $align='', $palign='', $border=0, $fitonpage=false);
     $this->pdf->ImageSVG('@' . $this->grafico_uno, $x=100, $y=0, $w='', $h=100, $link='', $align='', $palign='', $border=0, $fitonpage=false);
     $this->pdf->ImageSVG('@' . $this->grafico_uno, $x=100, $y=100, $w='', $h=100, $link='', $align='', $palign='', $border=0, $fitonpage=false);
