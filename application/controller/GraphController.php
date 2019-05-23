7e8f31d9124a77e9d90f5ba844b4e8149fde8efe
Cristopher Castro <cristophernic@live.cl>
2019-05-22 21:19:18 -0400

    .

--- application/controller/GraphController.php
+++ application/controller/GraphController.php
@@ -11,17 +11,20 @@ class GraphController extends Controller
     {
         $respuesta = RespuestaModel::getRespuesta($solicitud_id);
 
-        $this->View->renderWithoutHeaderAndFooter('pdf/finalinforme/index_grafico_ver', 
-        array(
-            'pdf' => new PdfModel(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false),
-            'solicitud' => SolicitudesModel::getSolicitud($solicitud_id,Session::get('user_email')),
-            'grafico_uno' => GraphModel::pesoFetal($respuesta->eg, $respuesta->pfe),
-            'grafico_dos' => GraphModel::ccca($respuesta->eg, $respuesta->ccca),
-            'grafico_tres' => GraphModel::uterinas($respuesta->eg, $respuesta->uterinas),
-            'grafico_cuatro' => GraphModel::umbilical($respuesta->eg, $respuesta->umbilical),
-            'grafico_cinco' => GraphModel::cerebralMedia($respuesta->eg, $respuesta->cm),
-            'grafico_seis' => GraphModel::cuocienteCerebroPlacentario($respuesta->eg, $respuesta->cmau)
-        ));
+        //$this->View->renderWithoutHeaderAndFooter('pdf/finalinforme/index_grafico_ver', 
+        //array(
+        //    'pdf' => new PdfModel(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false),
+        //    'solicitud' => SolicitudesModel::getSolicitud($solicitud_id,Session::get('user_email')),
+        //    'grafico_uno' => GraphModel::pesoFetal($respuesta->eg, $respuesta->pfe),
+        //    'grafico_dos' => GraphModel::ccca($respuesta->eg, $respuesta->ccca),
+        //    'grafico_tres' => GraphModel::uterinas($respuesta->eg, $respuesta->uterinas),
+        //    'grafico_cuatro' => GraphModel::umbilical($respuesta->eg, $respuesta->umbilical),
+        //    'grafico_cinco' => GraphModel::cerebralMedia($respuesta->eg, $respuesta->cm),
+        //    'grafico_seis' => GraphModel::cuocienteCerebroPlacentario($respuesta->eg, $respuesta->cmau)
+        //));
+        GraphModel::pesoFetal($respuesta->eg, $respuesta->pfe);
+        GraphModel::ccca($respuesta->eg, $respuesta->ccca);
+        GraphModel::uterinas($respuesta->eg, $respuesta->uterinas);
     }
 
 }
