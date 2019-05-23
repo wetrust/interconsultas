942b84687969efe5570381f9ab6ec4bd13803c53
Cristopher Castro <cristophernic@live.cl>
2019-05-22 20:38:35 -0400

    .

--- application/controller/GraphController.php
+++ application/controller/GraphController.php
@@ -16,7 +16,7 @@ class GraphController extends Controller
             'pdf' => new PdfModel(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false),
             'solicitud' => SolicitudesModel::getSolicitud($solicitud_id,Session::get('user_email')),
             'grafico_uno' => GraphModel::pesoFetal($respuesta->eg, $respuesta->pfe),
-            'grafico_dos' => GraphModel::ccca($respuesta->eg, $respuesta->ccca),
+            'grafico_dos' => GraphModel::uterinas($respuesta->eg, $respuesta->ccca),
             'grafico_tres' => GraphModel::uterinas($respuesta->eg, $respuesta->uterinas),
             'grafico_cuatro' => GraphModel::umbilical($respuesta->eg, $respuesta->umbilical),
             'grafico_cinco' => GraphModel::cerebralMedia($respuesta->eg, $respuesta->cm),
