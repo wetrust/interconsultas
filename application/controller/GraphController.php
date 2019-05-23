c5a66a26a4cfa4d5c30035fa326cb7d84d933091
Cristopher Castro <cristophernic@live.cl>
2019-05-22 21:27:25 -0400

    .

--- application/controller/GraphController.php
+++ application/controller/GraphController.php
@@ -22,9 +22,18 @@ class GraphController extends Controller
         //    'grafico_cinco' => GraphModel::cerebralMedia($respuesta->eg, $respuesta->cm),
         //    'grafico_seis' => GraphModel::cuocienteCerebroPlacentario($respuesta->eg, $respuesta->cmau)
         //));
-        //GraphModel::pesoFetal($respuesta->eg, $respuesta->pfe);
-        GraphModel::ccca($respuesta->eg, $respuesta->ccca);
-        //GraphModel::uterinas($respuesta->eg, $respuesta->uterinas);
+
+        $this->View->renderWithoutHeaderAndFooter('pdf/finalinforme/index_grafico', 
+        array(
+            'pdf' => new PdfModel(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false),
+            'solicitud' => SolicitudesModel::getSolicitud($solicitud_id,Session::get('user_email')),
+            'grafico_uno' => GraphModel::pesoFetal($respuesta->eg, $respuesta->pfe),
+            'grafico_dos' => GraphModel::ccca($respuesta->eg, $respuesta->ccca),
+            'grafico_tres' => GraphModel::uterinas($respuesta->eg, $respuesta->uterinas),
+            'grafico_cuatro' => GraphModel::umbilical($respuesta->eg, $respuesta->umbilical),
+            'grafico_cinco' => GraphModel::cerebralMedia($respuesta->eg, $respuesta->cm),
+            'grafico_seis' => GraphModel::cuocienteCerebroPlacentario($respuesta->eg, $respuesta->cmau)
+        ));
     }
 
 }
