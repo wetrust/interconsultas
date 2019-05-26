c4356ea9407decc18bfcac9d45e57e26c0a1ae95
Cristopher Castro <cristophernic@live.cl>
2019-05-25 23:16:18 -0400

    .

--- application/view/pdf/finalinforme/index.php
+++ application/view/pdf/finalinforme/index.php
@@ -50,7 +50,7 @@
     $html = '<table><tbody><tr><td>Fecha de solicitud: '.$solicitud_fecha.'</td><td>FUR Referida: '.$solicitud_fum.'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
-    $html = '<table><tbody><tr><td>Diagnóstico de referencia: '.htmlentities($this->solicitud->solicitud_diagnostico).'</td><td>Edad Gestacional solicitud: '.htmlentities($this->solicitud->solicitud_egestacional).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td>Edad Gestacional: '.htmlentities($this->solicitud->solicitud_egestacional).'</td><td>Diagnóstico de referencia: '.htmlentities($this->solicitud->solicitud_diagnostico).'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
     $html = '<table><tbody><tr><td>Ciudad procedencia: '.htmlentities($this->solicitud->solicitud_ciudad).'</td><td>Lugar de control: '.htmlentities($this->solicitud->solicitud_lugar).'</td></tr></tbody></table>';
