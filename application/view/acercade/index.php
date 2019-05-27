329007b07f582aaf54b90de8c7e99299ff3f5d80
Cristopher Castro <cristophernic@live.cl>
2019-05-26 20:20:42 -0400

    acerca de la plataforma

---
+++ application/view/acercade/index.php
@@ -0,0 +1,49 @@
+<div class="container">
+    <div class="row p-2">
+        <div class="col-6">
+            <div class="card">
+                <div class="card-block">
+                    <h4>Acerca de la plataforma</h4>
+                    <div class="row">
+                        <div class="col">
+                            <p class="text-justify mb-0"><small><strong>Consta de tres módulo para la evaluación del crecimiento fetal.</strong></small></p>
+                            <ul class="mb-0">
+                                <li><small>Determinación de la edad gestacional.</small></li>
+                                <li><small>Valoración ecográfica del crecimiento fetal.</small></li>
+                                <li><small>Evaluación de flujometria Doppler materno/fetal.</small></li>
+                            </ul>
+                            <p class="text-justify mb-0"><small><strong>La aplicación web dispone de dos herramientas informáticas.</strong></small></p>
+                            <ul>
+                                <li><small><a href="https://crecimientofetal.cl">Crecimientofetal.cl</a></small></li>
+                                <li><small><a href="https://crecimientofetal.cl/interconsulta">Crecimientofetal.cl/interconsulta</a></small></li>
+                            </ul>
+                            <p class="text-justify mb-0"><em>El primer software permite obtener informe de <strong>protocolo estandarizado</strong> y gráficas de crecimiento para valoración biométrica fetal y de flujometria Doppler materno fetal.</em></p>
+                            <p class="text-justify mb-0"><em>La segunda aplicación web permite  además  de forma resumida, el envío  mediante Email de la información obtenida al profesional referente, a objeto de <strong>optimizar el sistema de referencia y contrareferencia</strong> entre los distintos niveles de atención en salud materna.</em></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="card">
+                <div class="card-block">
+                    <h4>Acerca del autor</h4>
+                    <div class="row">
+                        <div class="col-2">
+                            <img class="d-block mx-auto img-fluid" src="https://servidor.crecimientofetal.cl/img/Dr_Lagos_copia.jpg" alt="logo">
+                        </div>
+                        <div class="col-10">
+                            <p><strong>Dr. Rudecindo Lagos Sandoval</strong></p>
+                            <p class="text-justify mb-0"><small>Médico cirujano, especialista en Ginecología y Obstetricia, docente de pre y postgrado. Profesor adscrito al Departamento de Obstetricia y Ginecología, Facultad de Medicina, Universidad de La Frontera, y Centro de Excelencia <a href="https://www.ciges.cl/index.php/quienes-somos/personal/academico/17-ciges-personas/ciges-academicos/47-dr-rudecindo-lagos-sandoval">CIGES</a>. Unidad de Ultrasonografía Gineco-Obstétrica de hospital base Hernán Hernríquez Aravena ciudad de Temuco, Región de La Arauncanía - Chile. <img src="https://servidor.crecimientofetal.cl/img/chile.jpg" alt="Chile"></small></p>
+                            <p><small>Línea de interés:</small></p>
+                            <ul class="mb-0">
+                                <li><small>Valoración del crecimiento intrauterino mediante exámen ultrasonográfico y flujometría Doppler.</small></li>
+                                <li><small>Valoración clinica del recién nacido. <a href="#postnatal">ver proyecto</a></small></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
