d0a3d5323b3c458b06f56b5706b7701da330e366
Cristopher Castro <cristophernic@live.cl>
2019-05-26 20:52:45 -0400

    .

--- application/view/acercade/index.php
+++ application/view/acercade/index.php
@@ -1,6 +1,6 @@
 <div class="container">
     <ol class="breadcrumb">
-        <li class="float-right"><a href="<?php echo Config::get('URL'); ?>"><strong>Volver</strong></a></li>
+        <li class="ml-auto"><a href="<?php echo Config::get('URL'); ?>"><strong>Volver</strong></a></li>
     </ol>
     <div class="row p-2">
         <div class="col-6">
