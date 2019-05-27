6deaed8a369fb63fa3117ef1ca6e6e9c7ecf0e0d
Cristopher Castro <cristophernic@live.cl>
2019-05-26 20:44:40 -0400

    .

--- application/view/acercade/index.php
+++ application/view/acercade/index.php
@@ -17,8 +17,8 @@
                                 <li><small><a href="https://crecimientofetal.cl">Crecimientofetal.cl</a></small></li>
                                 <li><small><a href="https://crecimientofetal.cl/interconsulta">Crecimientofetal.cl/interconsulta</a></small></li>
                             </ul>
-                            <p class="text-justify mb-0"><em>El primer software permite obtener informe de <strong>protocolo estandarizado</strong> y gráficas de crecimiento para valoración biométrica fetal y de flujometria Doppler materno fetal.</em></p>
-                            <p class="text-justify mb-0"><em>La segunda aplicación web permite  además  de forma resumida, el envío  mediante Email de la información obtenida al profesional referente, a objeto de <strong>optimizar el sistema de referencia y contrareferencia</strong> entre los distintos niveles de atención en salud materna.</em></p>
+                            <p class="text-justify mb-0"><small><em>El primer software permite obtener informe de <strong>protocolo estandarizado</strong> y gráficas de crecimiento para valoración biométrica fetal y flujometria Doppler materno fetal.</em></small></p>
+                            <p class="text-justify mb-0"><small><em>La segunda aplicación web permite  además  de forma resumida, enviar  mediante Email la información obtenida al profesional referente, a objeto de <strong>optimizar el sistema de referencia y contrareferencia</strong> entre los distintos niveles de atención en salud materna.</em></small></p>
                         </div>
                     </div>
                 </div>
@@ -35,10 +35,10 @@
                         <div class="col-10">
                             <p><strong>Dr. Rudecindo Lagos Sandoval</strong></p>
                             <p class="text-justify mb-0"><small>Médico cirujano, especialista en Ginecología y Obstetricia, docente de pre y postgrado. Profesor adscrito al Departamento de Obstetricia y Ginecología, Facultad de Medicina, Universidad de La Frontera, y Centro de Excelencia <a href="https://www.ciges.cl/index.php/quienes-somos/personal/academico/17-ciges-personas/ciges-academicos/47-dr-rudecindo-lagos-sandoval">CIGES</a>. Unidad de Ultrasonografía Gineco-Obstétrica de hospital base Hernán Hernríquez Aravena ciudad de Temuco, Región de La Arauncanía - Chile. <img src="https://servidor.crecimientofetal.cl/img/chile.jpg" alt="Chile"></small></p>
-                            <p><small>Línea de interés:</small></p>
+                            <p><small>Línea de interés y proyectos en desarrollo:</small></p>
                             <ul class="mb-0">
                                 <li><small>Valoración del crecimiento intrauterino mediante exámen ultrasonográfico y flujometría Doppler.</small></li>
-                                <li><small>Curvas de peso, valorar efecto modulador de variables biológicas materno/fetales en la categorización del peso al nacer.</small></li>
+                                <li><small>Curvas de crecimiento intrauterino, valorar efecto modulador de variables biológicas materno/fetales en la categorización del peso al nacer.</small></li>
                             </ul>
                         </div>
                     </div>
