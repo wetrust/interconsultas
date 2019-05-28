43e7894968e487682982eba401eef89c4ed54e94
Cristopher Castro <cristophernic@live.cl>
2019-05-28 16:20:25 -0400

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -36,7 +36,9 @@
         </a>
         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="user/editUsername">Modificar nombre</a>
-          <a class="dropdown-item" href="user/editProfesion">Modificar profesión</a>
+          <?php if (Session::get("user_account_type") < 3) { ?>
+          <a class="dropdown-item" href="user/editProfesion">Profesional referente</a>
+          <?php } ?>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="login/logout">Cerrar sesion</a>
         </div>
