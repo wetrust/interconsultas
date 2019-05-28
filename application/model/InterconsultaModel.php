b8b333d9298dc78b4c183096ba245c319b7b8b9c
Cristopher Castro <cristophernic@live.cl>
2019-05-28 16:52:13 -0400

    .

--- application/model/InterconsultaModel.php
+++ application/model/InterconsultaModel.php
@@ -51,7 +51,7 @@ class InterconsultaModel
                 }
                 
                 $solicitud_id = SolicitudesModel::createSolicitud($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_eg,$solicitud_eco,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional);
-                EvaluacionModel::createEvaluacion($solicitud_id, $solicitud_fecha, "");7
+                EvaluacionModel::createEvaluacion($solicitud_id, $solicitud_fecha, "");
                 SolicitudesModel::updateStateSolicitud($solicitud_id, 1);
                 $response->result = true;
             }
