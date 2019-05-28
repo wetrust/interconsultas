550d1471d13f4119c95a4321be6ec2b39eff5e46
Cristopher Castro <cristophernic@live.cl>
2019-05-27 21:00:05 -0400

    .

--- application/view/admin/index.php
+++ application/view/admin/index.php
@@ -24,7 +24,7 @@
                 <td><?= $user->user_email; ?></td>
                 <form action="<?= config::get("URL"); ?>admin/actionAccountSettings" method="post">
                 <td>
-                <?php $interests = array(1 => 'Invitado',  2 => 'Referente', 3 => 'Contrarreferente', 4 => 'Autoreferido');?>
+                <?php $interests = array(1 => 'Invitado',  2 => 'Referente', 3 => 'Contrarreferente', 4 => 'Autorreferido');?>
                     <select name="user_account_type" class="form-control">
                 <?php
                     foreach($interests as $k => $v) {
