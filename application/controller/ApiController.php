b838b843b35c6aa9c243eddc167800f9ac49a48c
Cristopher Castro <cristophernic@live.cl>
2019-05-28 16:39:02 -0400

    .

--- application/controller/ApiController.php
+++ application/controller/ApiController.php
@@ -4,8 +4,12 @@ class ApiController extends Controller
 {
     public function send()
     {
-        header('Access-Control-Allow-Origin: *');
         $this->View->renderJSON(InterconsultaModel::solicitarInterconsulta(Request::post('nombre_para'), Request::post('para'),Request::post('nombre'), Request::post('rut'), Request::post('fecha'),Request::post('eg'),Request::post('eco'),Request::post('diagnostico'),Request::post('lugar'),Request::post('ciudad'),Request::post('profesional'),Request::post('nombreprofesional'),Request::post('email'),Request::post('fum'),0,Request::post('egestacional')));
+    }
+
+    public function internal()
+    {
+        $this->View->renderJSON(InterconsultaModel::solicitarInterconsultaInternal(Request::post('nombre_para'), Request::post('para'),Request::post('nombre'), Request::post('rut'), Request::post('fecha'),Request::post('eg'),Request::post('eco'),Request::post('diagnostico'),Request::post('lugar'),Request::post('ciudad'),Request::post('profesional'),Request::post('nombreprofesional'),Request::post('email'),Request::post('fum'),0,Request::post('egestacional')));
     }
 
     public function profesionales(){
--- application/model/InterconsultaModel.php
+++ application/model/InterconsultaModel.php
@@ -33,4 +33,32 @@ class InterconsultaModel
         
         return $response;
     }
+
+    public static function solicitarInterconsultaInternal($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_eg,$solicitud_eco,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional)
+    {
+        //verificar si el profesional está habilitado para recibir interconsultas
+        $contrareferente = UserModel::getUserDataByUserNameOrEmail($solicitud_profesionalemail);
+        $response = new stdClass();
+
+        if (isset($contrareferente->user_email)){
+            if ($contrareferente->user_email == $solicitud_profesionalemail){
+                //almacenar la interconsulta
+
+                if (Session::userIsLoggedIn()) {
+                    $solicitud_profesional = UserModel::getPublicProfileOfUser(Session::get('user_id'))->user_profesion;
+                    $solicitud_nombreprofesional = Session::get('user_name');
+                    $solicitud_email = Session::get('user_email');
+                }
+                
+                $solicitud_id = SolicitudesModel::createSolicitud($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_eg,$solicitud_eco,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional);
+                EvaluacionModel::createEvaluacion($solicitud_id, $solicitud_fecha, "");
+                $response->result = true;
+            }
+        }
+        else{
+            $response->result = false;
+        }
+        
+        return $response;
+    }
 }
