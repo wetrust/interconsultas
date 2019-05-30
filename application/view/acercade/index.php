f190af8057b48f919195851b32933358d34dadb3
Cristopher Castro <cristophernic@live.cl>
2019-05-29 22:02:04 -0400

    .

--- application/view/acercade/index.php
+++ application/view/acercade/index.php
@@ -9,19 +9,18 @@
                     <h4>Acerca de la plataforma</h4>
                     <div class="row">
                         <div class="col">
-                            <p class="text-justify mb-0"><small><strong>Consta de tres módulo para la evaluación del crecimiento fetal.</strong></small></p>
-                            <ul class="mb-0">
-                                <li><small>Determinación de la edad gestacional.</small></li>
-                                <li><small>Valoración ecográfica del crecimiento fetal.</small></li>
-                                <li><small>Evaluación de flujometria Doppler materno/fetal.</small></li>
-                            </ul>
-                            <p class="text-justify"><br><small><strong>La aplicación web dispone de dos herramientas informáticas.</strong></small></p>
+                            <p class="text-justify">El sitio web <a href="https://crecimientofetal.cl">Crecimientofetal.cl</a>,  permite valoración del crecimiento fetal mediante <strong>protocolo estandarizado</strong>, generando informes  y graficas, la aplicación consta de tres módulos:</p>
                             <ul>
-                                <li><small><a href="https://crecimientofetal.cl/interconsulta">Crecimientofetal.cl/interconsulta</a></small></li>
+                                <li>Determinación de la edad gestacional.</li>
+                                <li>Valoración ecográfica del crecimiento.</li>
+                                <li>Evaluación de flujometria Doppler materno/fetal.</li>
                             </ul>
-                            <p class="text-justify mb-0"><small><em>El primer software permite obtener informe de <strong>protocolo estandarizado</strong> y gráficas de crecimiento para valoración biométrica fetal y flujometria Doppler materno fetal.</em></small></p>
-                            <p class="text-justify mb-0"><small><em>La segunda aplicación web permite  además  de forma resumida, enviar  mediante Email la información obtenida al profesional referente, a objeto de <strong>optimizar el sistema de referencia y contrareferencia</strong> entre los distintos niveles de atención en salud materna.</em></small></p>
-                            <p class="text-justify mb-0"><small>La plataforma consta de 3 categorías de usuario: Invitado, Referente y Contrarreferente.</small></p>
+                            <p class="text-justify">El anexo <a href="https://crecimientofetal.cl/interconsulta">Crecimientofetal.cl/interconsulta</a> permite además enviar mediante Email la información obtenida al profesional referente, a objeto de <strong>optimizar el sistema de referencia y contrarreferencia</strong> entre los distintos niveles de atención en salud materna. La plataforma consta de tres categorías de usuario:</p>
+                            <ul>
+                                <li>Invitado: puede ver y solicitar interconsulta al profesional contrarreferente.</li>
+                                <li>Referente: solicitar interconsulta, almacenar informes en base de datos.Valoración ecográfica del crecimiento.</li>
+                                <li>Contrarreferente: <strong>realizar exámenes</strong> y almacenamiento en base de datos.</li>
+                            </ul>
                         </div>
                     </div>
                 </div>
