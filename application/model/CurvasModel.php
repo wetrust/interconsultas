5f8693ea3eb30c7667ab52fdebf75e0c5b021d60
Cristopher Castro <cristophernic@live.cl>
2019-05-30 19:26:48 -0400

    .

--- application/model/CurvasModel.php
+++ application/model/CurvasModel.php
@@ -4,24 +4,24 @@ class CurvasModel
 {
     public static function cerebelo($eg, $cerebelo)
     {
-        $Tabla = DataModel::cerebelo();
+        $tabla = DataModel::cerebelo();
 
         if ($eg < 15 || $eg > 40) {
             return 0;
         }
         else {
-            $uno = Tabla[2][eg] - Tabla[0][eg];
-            $dos = $cerebelo - Tabla[0][eg];
-            $resultado = parseInt(95 / (uno) * (dos) + 5);
+            $uno = $tabla[2][eg] - $tabla[0][eg];
+            $dos = $cerebelo - $tabla[0][eg];
+            $resultado = parseInt(95 / ($uno) * ($dos) + 5);
 
-            if (resultado > 99) {
+            if ($resultado > 99) {
                 return '> 99';
             }
-            else if (resultado < 1) {
+            else if ($resultado < 1) {
                 return '< 1';
             }
             else {
-                return resultado;
+                return $resultado;
             }
         }
     }
