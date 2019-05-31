855dddc4fb28248cbb6073f35701e7c97ec5a0d7
Cristopher Castro <cristophernic@live.cl>
2019-05-30 21:02:35 -0400

    .

--- application/view/user/editUserEmail.php
+++ application/view/user/editUserEmail.php
@@ -1,14 +1,16 @@
 <div class="container">
-    <h1>UserController/editUserEmail</h1>
+    <ol class="breadcrumb">
+        <li class="ml-auto"><a href="<?php echo Config::get('URL'); ?>"><strong>Volver</strong></a></li>
+    </ol>
+    <h1>Cambiar correo electrónico de usuario</h1>
     <!-- echo out the system feedback (error and success messages) -->
     <?php $this->renderFeedbackMessages(); ?>
-    <h2>Change your email address</h2>
     <form action="<?php echo Config::get('URL'); ?>user/editUserEmail_action" method="post">
         <div class="form-group">
-            <label for="exampleInputEmail1">New email address:</label>
+            <label for="exampleInputEmail1">Escriba el nuevo correo electrónico de usuario:</label>
             <input type="email" class="form-control" name="user_email" aria-describedby="emailHelp" placeholder="Enter email" required>
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <small id="emailHelp" class="form-text text-muted">No compartimos su correo y lo guardamos con total seguridad.</small>
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Guardar correo</button>
     </form>
 </div>
