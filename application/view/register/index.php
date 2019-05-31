9d385d94cd842daf6796d6e3b874f834ac89583f
Cristopher Castro <cristophernic@live.cl>
2019-05-30 21:05:18 -0400

    .

--- application/view/register/index.php
+++ application/view/register/index.php
@@ -2,32 +2,24 @@
     <div class="d-flex p-2 justify-content-center">
         <div class="card">
             <div class="card-body">
-                <h5 class="card-title">Register a new account</h5>
+                <h5 class="card-title">Registrar una nueva cuenta de usuario</h5>
                 <?php $this->renderFeedbackMessages(); ?>
                 <form action="<?php echo Config::get('URL'); ?>register/register_action" method="post">
                     <div class="form-group">
-                        <label>Username (letters/numbers, 2-64 chars)</label>
+                        <label>Nombre</label>
                         <input class="form-control" type="text" name="user_name" pattern="[a-zA-Z0-9 ]{2,64}" required />
                     </div>
                     <div class="form-group">
-                        <label>email address (a real address)</label>
+                        <label>Correo electrónico (un correo real)</label>
                         <input class="form-control" type="text" name="user_email" required />
                     </div>
                     <div class="form-group">
-                        <label>repeat email address (to prevent typos)</label>
-                        <input class="form-control" type="text" name="user_email_repeat" required />
-                    </div>
-                    <div class="form-group">
-                        <label>Password (6+ characters)</label>
+                        <label>Contraseña (6 o más carácteres)</label>
                         <input class="form-control" type="password" name="user_password_new" pattern=".{6,}" required />
-                    </div>
-                    <div class="form-group">
-                        <label>Repeat your password</label>
-                        <input class="form-control" type="password" name="user_password_repeat" pattern=".{6,}" required />
                     </div>
                     <img class="img-fluid img-thumbnail" id="captcha" src="<?php echo Config::get('URL'); ?>register/showCaptcha" />
                     <div class="form-group">
-                        <label>Enter captcha above</label>
+                        <label>Escriba la captcha</label>
                         <input class="form-control" type="text" name="captcha" required />
                         <a href="#" onclick="document.getElementById('captcha').src = '<?php echo Config::get('URL'); ?>register/showCaptcha?' + Math.random(); return false">Reload Captcha</a>
                     </div>
