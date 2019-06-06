8053cff444b5af17190e9d8730c86b6a83b04c51
Cristopher Castro <cristophernic@live.cl>
2019-06-05 20:05:46 -0400

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -61,11 +61,11 @@
         <div class="tab-pane fade" id="consentimiento" role="tabpanel" aria-labelledby="consentimiento-tab">
             <div class="card my-2 shadow">
                 <div class="card-body">
-                    <p>Según la legislación vigente, la información recopilada en base de datos es solo con fines de conocer el historial clínico del paciente.</p>
-                    <p class="text-danger">El uso de la información total o parcial, para fines de estudios requiere de autorización escrita por parte del paciente.</p>
-                    <p>Para tal efecto se requiere de “consentimiento informado”, siguiendo protocolos estándares de los comité científicos respectivos.</p>
-                    <div class="row mt-4">
-                        <div class="col-8">Protocolo para para estudio de modulo prenatal ultrasonográfico, datos ultrasonográficos.</div>
+                    <p><strong><em>Según la legislación vigente, la información recopilada en base de datos es solo con fines de conocer el historial clínico del paciente.</em></strong></p>
+                    <p class="text-danger"><strong><em>El uso de la información total o parcial, para fines de estudios requiere de autorización escrita por parte del paciente.</em></strong></p>
+                    <p><strong><em>Para tal efecto se requiere de “consentimiento informado”, siguiendo protocolos estándares de los comité científicos respectivos.</em></strong></p>
+                    <div class="row mt-5">
+                        <div class="col-8"><p><strong><em>Protocolo para para estudio de modulo prenatal ultrasonográfico, datos ultrasonográficos.</em></strong></p></div>
                         <div class="col-2">
                             <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="uno" value="option1">
@@ -78,8 +78,8 @@
                         </div>
                         <div class="col-2"><a class="btn btn-primary" href="#">Formulario</a></div>
                     </div>
-                    <div class="row mt-2">
-                        <div class="col-8">Protocolo para para estudio de modulo postnatal datos clínicos de parto y recién nacido.</div>
+                    <div class="row mt-2 mb-5">
+                        <div class="col-8"><p><strong><em>Protocolo para para estudio de modulo postnatal datos clínicos de parto y recién nacido.</em></strong></div>
                         <div class="col-2">
                             <div class="form-check form-check-inline">
                                 <input class="form-check-input" type="radio" name="dos" value="option1">
@@ -93,7 +93,7 @@
                         <div class="col-2"><a class="btn btn-primary" href="#">Formulario</a></div>
                     </div>
                     <div class="form-group">
-                        <label>Comentarios y observaciones</label>
+                        <label><strong><em>Comentarios y observaciones</em></strong></label>
                         <input type="text" class="form-control">
                     </div>
                 </div>
