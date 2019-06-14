d30c0fc47b7ff05c973c0ecd1ad094c3674df164
Cristopher Castro <cristophernic@live.cl>
2019-06-14 19:44:57 -0400

    .

--- public/js/solicitud.js
+++ public/js/solicitud.js
@@ -133,7 +133,13 @@ $(document).ready(function(){
 		}
     });
     
-    loadContrarreferentes();
+	loadContrarreferentes();
+	
+	var now = new Date();
+	var day = ("0" + now.getDate()).slice(-2);
+	var month = ("0" + (now.getMonth() + 1)).slice(-2);
+	var today = now.getFullYear()+"-"+(month)+"-"+(day) ;
+	$("#interconsulta\\.fecha").val(today);
 });
 
 function construir(){
