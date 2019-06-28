8ee166a9d2d2172c8b385a6755b05dcba656e4f1
Cristopher Castro <cristophernic@live.cl>
2019-06-27 20:55:22 -0400

    .

--- application/view/pdf/finalinforme/parto_ver.php
+++ application/view/pdf/finalinforme/parto_ver.php
@@ -56,10 +56,10 @@
     $html = '<table><tbody><tr><td>Tipo de parto: '.htmlentities($this->parto->ipn).'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(2);
-    $this->pdf->ImageSVG('@' . $this->grafico_uno, $x=10, $y=60, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
-    $this->pdf->ImageSVG('@' . $this->grafico_dos, $x=105, $y=60, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
-    $this->pdf->ImageSVG('@' . $this->grafico_tres, $x=10, $y=169, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
-    $this->pdf->ImageSVG('@' . $this->grafico_cuatro, $x=105, $y=169, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
+    $this->pdf->ImageSVG('@' . $this->grafico_uno, $x=10, $y=65, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
+    $this->pdf->ImageSVG('@' . $this->grafico_dos, $x=105, $y=65, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
+    $this->pdf->ImageSVG('@' . $this->grafico_tres, $x=10, $y=149, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
+    $this->pdf->ImageSVG('@' . $this->grafico_cuatro, $x=105, $y=149, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
     
     //para enviar por email
     //$tmp = Config::get('PATH_AVATARS');
