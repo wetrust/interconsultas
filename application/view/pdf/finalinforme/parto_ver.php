5478ffb4b9137fa22e849df4620f3ee9d36405fc
Cristopher Castro <cristophernic@live.cl>
2019-06-27 21:14:49 -0400

    .

--- application/view/pdf/finalinforme/parto_ver.php
+++ application/view/pdf/finalinforme/parto_ver.php
@@ -8,11 +8,9 @@
 
     // set default header data
     //$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 009', PDF_HEADER_STRING);
-
     // set header and footer fonts
     //$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
     //$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
     // set default monospaced font
     $this->pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
 
@@ -31,42 +29,45 @@
     $fecha = explode("-", $this->parto->fecha_parto);
     $fecha = $fecha[2] . "-". $fecha[1]. "-". $fecha[0];
 
+    $this->pdf->Ln(5);
     $html = '<h2 style="border-bottom:2px double #000;text-align: center;">INFORME DATOS DE PARTO</h2>';
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'C', true);
     $this->pdf->Ln(2);
 
-    $html = '<table><tbody><tr><td>Nombre del paciente: '.htmlentities($this->paciente->solicitud_nombre).'</td><td>RUT (DNI): '.htmlentities($this->paciente->solicitud_rut).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td><strong>Datos maternos</strong></td><td><strong>Datos del recien nacido</strong></td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
-    $html = '<table><tbody><tr><td>Fecha de parto: '.$fecha.'</td><td>Edad gestacional al parto: '.$this->parto->semanas. "," .$this->parto->dias.' semanas</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td>Nombre de la madre: '.htmlentities($this->paciente->solicitud_nombre).'</td><td>Sexo: '.htmlentities($this->parto->sexo).'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
-    $html = '<table><tbody><tr><td>Peso: '.htmlentities($this->parto->pesofetal).' kg.</td><td>Peso / EG: '.htmlentities($this->parto->peso_eg).', '.htmlentities($this->parto->peso_eg_estado).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td>RUT (DNI): '.htmlentities($this->paciente->solicitud_rut).'</td><td>Peso: '.htmlentities($this->parto->pesofetal).' kg.</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
-    $html = '<table><tbody><tr><td>Talla: '.htmlentities($this->parto->tallafetal).' cms.</td><td>IPN / EG: '.htmlentities($this->parto->ipn_eg). ', '.htmlentities($this->parto->ipn_eg_estado).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td>Lugar de parto: '.htmlentities($this->paciente->solicitud_rut).'</td><td>Talla: '.htmlentities($this->parto->tallafetal).' cms.</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
-    $html = '<table><tbody><tr><td>Cráneo: '.htmlentities($this->parto->craneofetal).'</td><td>Sexo: '.htmlentities($this->parto->sexo).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td>Tipo de parto: '.htmlentities($this->paciente->solicitud_rut).'</td><td>Cráneo: '.htmlentities($this->parto->craneofetal).'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
-    $html = '<table><tbody><tr><td>IPN: '.htmlentities($this->parto->ipn).'</td><td>Protocolo Hipoglicemia: '.htmlentities($this->parto->tipo).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td>Paridad: '.htmlentities($this->paciente->solicitud_rut).'</td><td>Peso / Ege: '.htmlentities($this->parto->peso_eg).', '.htmlentities($this->parto->peso_eg_estado).'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
-    $html = '<table><tbody><tr><td>Tipo de parto: '.htmlentities($this->parto->ipn).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td>Fecha de parto: '.$fecha.'</td><td>IPN / Ege: '.htmlentities($this->parto->ipn_eg). ', '.htmlentities($this->parto->ipn_eg_estado).'</td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->Ln(1);
+    $html = '<table><tbody><tr><td>Edad gestacional al parto: '.$this->parto->semanas. "," .$this->parto->dias.' semanas</td><td>Protocolo Hipoglicemia: '.htmlentities($this->parto->tipo).'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(2);
     $this->pdf->ImageSVG('@' . $this->grafico_uno, $x=10, $y=65, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
     $this->pdf->ImageSVG('@' . $this->grafico_dos, $x=105, $y=65, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
     $this->pdf->ImageSVG('@' . $this->grafico_tres, $x=10, $y=140, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
     $this->pdf->ImageSVG('@' . $this->grafico_cuatro, $x=105, $y=140, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
-    
+
     //para enviar por email
     //$tmp = Config::get('PATH_AVATARS');
     //if (file_exists("$tmp/informe.pdf")) unlink("$tmp/informe.pdf");
     //$this->pdf->Output("$tmp/informe.pdf", "F");
     /////////
-
     //para visualizar en el navegador
     $this->pdf->Output('Informe.pdf', 'I');
     //$base64 = chunk_split(base64_encode($this->pdf->Output('Informe.pdf', 'S')));
