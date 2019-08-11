6620b2403ee78722007a468b3004375de70a924c
Cristopher Castro <cristophernic@live.cl>
2019-08-10 22:19:07 -0400

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -257,7 +257,7 @@
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Enviar informe por email</h5>
+                <h5 class="modal-title" id="exampleModalLabel">Enviar informe y gráfica por email</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
