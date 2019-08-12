5733f433279271f6b366806c77276996ff3a37e4
Cristopher Castro <cristophernic@live.cl>
2019-08-11 21:04:41 -0400

    .

--- application/view/pdf/finalinforme/primertrimestre.php
+++ application/view/pdf/finalinforme/primertrimestre.php
@@ -101,7 +101,7 @@
     $this->pdf->Ln(1);
 
     if (is_null($this->respuesta_lcn) == false){
-        $html = '<table><tbody><tr><td>LCN : '. $this->respuesta_lcn.'</td><td><strong>Edad Gestacional según LCN:</strong> '. $this->respuesta_lcn_eg.' semanas*</td></tr></tbody></table>';
+        $html = '<table><tbody><tr><td>LCN : '.gettype($this->respuesta_lcn). "/".$this->respuesta_lcn.'</td><td><strong>Edad Gestacional según LCN:</strong> '. $this->respuesta_lcn_eg.' semanas*</td></tr></tbody></table>';
         $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, '', true);
         $this->pdf->Ln(1);
 
