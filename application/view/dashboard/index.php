f5feb0768450e63c6489a519ada72a98257eb831
Cristopher Castro <cristophernic@live.cl>
2019-08-13 01:52:39 -0400

    .

--- application/view/dashboard/index.php
+++ application/view/dashboard/index.php
@@ -31,7 +31,7 @@
                 <ul class="nav nav-tabs" id="myTab" role="tablist">
                     <li class="nav-item"><a class="nav-link active" id="interconsulta-tab" data-toggle="tab" href="#interconsulta" role="tab" aria-controls="interconsulta" aria-selected="true"><small>Módulo prenatal, exámenes ecográficos</small></a></li>
                     <li class="nav-item"><a class="nav-link" id="parto-tab" data-toggle="tab" href="#parto" role="tab" aria-controls="parto" aria-selected="false"><small>Módulo postnatal, datos del parto y recién nacido</small></a></li>
-                    <li class="nav-item d-none"><a class="nav-link" id="directorio-tab" data-toggle="tab" href="#directorio" role="tab" aria-controls="directorio" aria-selected="false"><small>Directorio</small></a></li>
+                    <li class="nav-item d-none"><a class="nav-link" id="directorio-tab" data-toggle="tab" href="#directorio" role="tab" aria-controls="directorio" aria-selected="false"><small>Lista de destinatario</small></a></li>
                 </ul>
             </div>
             <div class="col">
