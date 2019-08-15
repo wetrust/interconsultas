997f513ada99ece5e01c4f6a17c7ebdf8ccf6e46
Cristopher Castro <cristophernic@live.cl>
2019-08-14 22:32:47 -0400

    .

--- application/view/pdf/finalinforme/primertrimestre_ver.php
+++ application/view/pdf/finalinforme/primertrimestre_ver.php
@@ -172,9 +172,10 @@
 
     if ($edadGestacional < 84){
         //determinar cuantos días faltan para las 12 semanas
-        $edadGestacional = 84- $edadGestacional;
+        $edadGestacional = 84 - $edadGestacional;
         //sumar esos días a la fecha de exámen
-        $solicitud_fecha_examen =  date('d-m-Y', strtotime($this->solicitud->solicitud_fecha. ' + '.$edadGestacional.' days'));
+        //$solicitud_fecha_examen =  date('d-m-Y', strtotime($this->solicitud->solicitud_fecha. ' + '.$edadGestacional.' days'));
+        $solicitud_fecha_examen =  $this->solicitud->solicitud_fecha. ' + '.$edadGestacional.' days';
 
 
         $_html = 'Eco 11-14 corresponde a 12 semanas +- 7 dias, realizar eco 11-14 en fecha '.$solicitud_fecha_examen.'<br>';
