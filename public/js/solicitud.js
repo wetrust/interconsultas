25c3ab341a42ada67bfa5b0fbf68a5d1c79c125d
Cristopher Castro <cristophernic@live.cl>
2019-08-24 23:55:30 -0400

    calcular presion arterial

--- public/js/solicitud.js
+++ public/js/solicitud.js
@@ -47,88 +47,165 @@ $(document).ready(function(){
 		}
     });
 
+	$("#k").on("change", function(){
+		var A = $("#k").val();
+		var B = $("#l").val();
+		if (Number.isInteger(A) && Number.isInteger(B)){
+			$("#ll").val((parseInt(A)+parseInt(B))/2);
+		}
+	});
+
+	$("#l").on("change", function(){
+		var A = $("#k").val();
+		var B = $("#l").val();
+		if (Number.isInteger(A) && Number.isInteger(B)){
+			$("#ll").val((parseInt(A)+parseInt(B))/2);
+		}
+	});
+
+
     $("#s").on("click", function(){
 		$(this).trigger("change");
 	}).on("change", function(){
 		let correo = $(this).val();
 		let nombre = $("#s option:selected").text();
-
 		$("#t").val(nombre);
 		$("#u").val(correo);
 	});
 
-    $("#interconsulta\\.enviar").on("click", function(){
+    $("#v").on("click", function(){
 		var listo = false;
 		//revisar si el usuario lleno todas las cajas
-		var nombre = String($("#interconsulta\\.nombre").val());
-		var rut = String($("#interconsulta\\.rut").val());
-		var fecha = String($("#interconsulta\\.fecha").val());
-		var telefono = String($("#interconsulta\\.telefono").val());
-		var eg = String($('input[name=interconsulta_eg]:checked').val());
-		var alteraciones = $("#interconsulta\\.alteraciones").val();
-		var fum = String($("#interconsulta\\.fum").val());
-		var diagnostico = String($("#interconsulta\\.diagnostico").val());
-		var lugar = String($("#interconsulta\\.lugar").val());
-		var ciudad = String($("#interconsulta\\.ciudad").val());
-		var egestacional = String($("#interconsulta\\.egestacional").val());
-		var para = String($("#interconsulta\\.para").val());
-		var nombre_para = String($("#interconsulta\\.para\\.nombre").val());
+		var nombre = String($("#a").val()); //es obligatoria
+		var rut = String($("#b").val()); //es obligatoria
+		var telefono = String($("#c").val()); //es obligatoria
+		var fum = String($("#d").val()); //es obligatoria
+		var fecha = String($("#e").val());
+		var eg = String($('#f').val()); //es obligatoria
+		var edadMaterna = String($('#g').val()); //es obligatoria
+		var ciudad = String($("#h").val()); //es obligatoria
+		var lugar = String($("#i").val()); //es obligatoria
+		var diagnostico = String($("#j").val()); //es obligatoria
+
+		var sistolica = $("#k").val();
+		var diastolica = String($("#l").val());
+		var media = String($("#ll").val());
+		var talla = String($("#m").val());
+		var peso = String($("#n").val());
+		var imc = String($("#o").val());
+		var antecedentes = String($("#p").val());
+		var nombreReferente = String($("#q").val());
+		var correoReferente = String($("#r").val());
+		var nombreContrarreferente = String($("#t").val());
+		var correoContrarreferente = String($("#u").val());
+
 
         var baseModal = '<div class="modal" tabindex="-1" role="dialog" id="cautivo.dialogo"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header">';
         var footerModal = '</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button></div></div></div></div>';
-        
-        if (nombre.length < 3 || rut.length < 4 || telefono.length < 6 || fecha.length < 4 || eg == 'undefined' || eg.length < 1 || fum.length < 4 || diagnostico.length  < 3 || ciudad.length < 2 || lugar.length  < 3 || egestacional.length < 3 || nombre_para.length < 2 || para.length < 5){
-            var mensaje = "";
+		
+		if (nombre.length < 3 || rut.length < 4 || telefono.length < 6 || fum.length < 4 || fecha.length < 4 || eg.length < 1 || ciudad.length < 2 || lugar.length  < 3 || diagnostico.length  < 3 || nombreReferente.length < 3 || correoReferente.length < 2){
+			var mensaje = "";
 
             if (nombre.length < 3){
                 mensaje = textos.paciente_name_error;
             }else if (rut.length < 4){
 				mensaje = textos.paciente_rut_error;
 			}else if (telefono.length < 6){
-                mensaje = textos.telefono_error;
-            }else if (fecha.length < 4){
-                mensaje = textos.form_error;
-            }else if (eg == 'undefined' || eg.length < 1){
-                mensaje = textos.eg_error;
-            }else if (fum.length < 4){
+				mensaje = textos.telefono_error;
+			}else if (fum.length < 4){
                 mensaje = textos.fur_error;
-            }else if (diagnostico.length  < 3){
-                mensaje = textos.diagnostico_referencia_error;
-            }else if (ciudad.length < 2){
+			}else if (fecha.length < 4){
+                mensaje = textos.form_error;
+            }else if (eg.length < 1){
+                mensaje = textos.eg_error;
+			}else if (ciudad.length < 2){
                 mensaje = textos.procedencia_error;
             }else if (lugar.length  < 3){
-                mensaje = textos.lugar_control_error;
-            }else if (egestacional.length < 3){
-                mensaje = textos.fechas_error;
-            }else if (nombre_para.length < 2){
-                mensaje = textos.referente_error;
-            }else if (para.length < 5){
-                mensaje = textos.contrarreferente_error;
-            }
-            $('body').append(baseModal + '<h5 class="modal-title">' + textos.form_error+'</h5></div><div class="modal-body"><p>'+ mensaje+'</p>'+ footerModal);
+				mensaje = textos.lugar_control_error;
+			}else if (diagnostico.length  < 3){
+                mensaje = textos.diagnostico_referencia_error;
+            }else if (nombreReferente.length < 3){
+                mensaje = textos.nombreReferente;
+            }else if (correoReferente.length < 2){
+                mensaje = textos.correoReferente;
+			}
+			
+            $('body').append(baseModal + '<h5 class="modal-title">' + textos.form_error+ '</h5></div><div class="modal-body"><p>'+ mensaje +'</p>'+ footerModal);
             $('#cautivo\\.dialogo').modal("show").on('hidden.bs.modal', function (e) { $(this).remove(); });
             return;
-        } else{ listo = true;}
+		}else if (a == 4 && nombreContrarreferente.length < 5 || correoContrarreferente.length < 5){
+			var mensaje = "";
+
+            if (nombreContrarreferente.length < 3){
+                mensaje = textos.nombreContrarreferente;
+            }else if (correoContrarreferente.length < 4){
+				mensaje = textos.correoContrarreferente;
+			}
+
+            $('body').append(baseModal + '<h5 class="modal-title">' + textos.form_error+ '</h5></div><div class="modal-body"><p>'+ mensaje +'</p>'+ footerModal);
+            $('#cautivo\\.dialogo').modal("show").on('hidden.bs.modal', function (e) { $(this).remove(); });
+            return;
+		}else{
+			listo = true;
+		}
 
 		if (listo == true){
-			$('#interconsulta\\.enviar').prop("disabled", true);
-			var eLdiagnostico = $("#interconsulta\\.diagnostico").val();
+			$('#v').prop("disabled", true);
 
-			var data = {
-				nombre: $("#interconsulta\\.nombre").val(),
-				rut: $("#interconsulta\\.rut").val(),
-				telefono: $("#interconsulta\\.telefono").val(),
-				fecha: $("#interconsulta\\.fecha").val(),
-				eg: $('input[name=interconsulta_eg]:checked').val(),
-				alteraciones: alteraciones,
-				fum: $("#interconsulta\\.fum").val(),
-				diagnostico: eLdiagnostico,
-				lugar: $("#interconsulta\\.lugar").val(),
-				ciudad: $("#interconsulta\\.ciudad").val(),
-				egestacional: $("#interconsulta\\.egestacional").val(),
-				para: $("#interconsulta\\.para").val(),
-				nombre_para: $("#interconsulta\\.para\\.nombre").val()
-			};
+			var data = {};
+
+			if (a == 4){
+				data = {
+					nombre: nombre,
+					rut: rut,
+					telefono: telefono,
+					fum: fum,
+					fecha: fecha,
+					eg: eg,
+					edadMaterna: edadMaterna,
+					ciudad: ciudad,
+					lugar: lugar,
+					diagnostico: diagnostico,
+					sistolica: sistolica,
+					diastolica: diastolica,
+					sistolica: sistolica,
+					diastolica: diastolica,
+					media: media,
+					talla: talla,
+					peso: peso,
+					imc: imc,
+					antecedentes: antecedentes,
+					nombreReferente: nombreReferente,
+					correoReferente: correoReferente,
+					nombreContrarreferente: nombreContrarreferente,
+					correoContrarreferente: correoContrarreferente
+				};
+			}else{
+				data = {
+					nombre: nombre,
+					rut: rut,
+					telefono: telefono,
+					fum: fum,
+					fecha: fecha,
+					eg: eg,
+					edadMaterna: edadMaterna,
+					ciudad: ciudad,
+					lugar: lugar,
+					diagnostico: diagnostico,
+					sistolica: sistolica,
+					diastolica: diastolica,
+					sistolica: sistolica,
+					diastolica: diastolica,
+					media: media,
+					talla: talla,
+					peso: peso,
+					imc: imc,
+					antecedentes: antecedentes,
+					nombreReferente: nombreReferente,
+					correoReferente: correoReferente
+				};
+			}
+
 			$('body').append(baseModal + textos.form_send + '</h5></div><div class="modal-body"><p>Enviando solicitud de interconsulta, por favor espere</p><div class="progress"><div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 100%"></div></div></div></div></div></div>');
 			$('#cautivo\\.dialogo').modal("show");
 		
