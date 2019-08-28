e131818652a02a00297b1334aefbab4dab6f4be7
Cristopher Castro <cristophernic@live.cl>
2019-08-27 20:59:25 -0400

    .

--- application/model/InterconsultaModel.php
+++ application/model/InterconsultaModel.php
@@ -2,7 +2,7 @@
 
 class InterconsultaModel
 {
-    public static function solicitarInterconsulta($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_egestacional,$telefono,$sistolica,$diastolica,$media,$talla,$peso,$imc,$antecedentes){
+    public static function solicitarInterconsulta($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_egestacional,$telefono,$sistolica,$diastolica,$media,$talla,$peso,$imc,$antecedentes, $edadMaterna){
         //verificar si el profesional está habilitado para recibir interconsultas
         $contrareferente = UserModel::getUserDataByUserNameOrEmail($solicitud_profesionalemail);
         $response = new stdClass();
@@ -14,7 +14,7 @@ class InterconsultaModel
                     $solicitud_nombreprofesional = Session::get('user_name');
                     $solicitud_email = Session::get('user_email');
                 }
-                $solicitud_id = SolicitudesModel::createSolicitud($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,0,$solicitud_egestacional,$telefono,$sistolica,$diastolica,$media,$talla,$peso,$imc,$antecedentes);
+                $solicitud_id = SolicitudesModel::createSolicitud($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,0,$solicitud_egestacional,$telefono,$sistolica,$diastolica,$media,$talla,$peso,$imc,$antecedentes, $edadMaterna);
                 //enviar un email al médico contrareferente
                 EmailModel::sendContrareferenteEmail($solicitud_id);
                 EmailModel::sendSolicitanteEmail($solicitud_id);
@@ -26,14 +26,14 @@ class InterconsultaModel
         return $response;
     }
 
-    public static function solicitarInterconsultaInternal($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_fum,$solicitud_egestacional,$telefono,$sistolica,$diastolica,$media,$talla,$peso,$imc,$antecedentes){
+    public static function solicitarInterconsultaInternal($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_fum,$solicitud_egestacional,$telefono,$sistolica,$diastolica,$media,$talla,$peso,$imc,$antecedentes, $edadMaterna){
         $response = new stdClass();
         $contrareferente = UserModel::getUserDataByUserNameOrEmail($solicitud_profesionalemail);
         $solicitud_profesional = UserModel::getPublicProfileOfUser(Session::get('user_id'))->user_profesion;
         $solicitud_nombreprofesional = Session::get('user_name');
         $solicitud_email = Session::get('user_email');
         $solicitud_respuesta = 0;
-        $solicitud_id = SolicitudesModel::createSolicitud($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional, $telefono, $sistolica, $diastolica, $media, $talla, $peso, $imc, $antecedentes);
+        $solicitud_id = SolicitudesModel::createSolicitud($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional, $telefono, $sistolica, $diastolica, $media, $talla, $peso, $imc, $antecedentes, $edadMaterna);
         EvaluacionModel::createEvaluacion($solicitud_id, $solicitud_fecha, "");
         SolicitudesModel::updateStateSolicitud($solicitud_id, 1);
         $response->result = true;
