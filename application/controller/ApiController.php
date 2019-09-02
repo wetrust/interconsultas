5d1c0940c5876650d46841fd42779eb7749cac1a
Cristopher Castro <cristophernic@live.cl>
2019-09-01 22:46:18 -0400

    .

--- application/controller/ApiController.php
+++ application/controller/ApiController.php
@@ -7,7 +7,31 @@ class ApiController extends Controller
        if (Session::get('user_account_type') == 4){
         $this->View->renderJSON(InterconsultaModel::solicitarInterconsultaInternal(Request::post('nombreReferente'), Request::post('correoReferente'),Request::post('nombre'), Request::post('rut'), Request::post('fecha'),Request::post('diagnostico'),Request::post('lugar'),Request::post('ciudad'),Request::post('fum'),Request::post('eg'),Request::post('telefono'),Request::post('sistolica'),Request::post('diastolica'),Request::post('media'),Request::post('talla'),Request::post('peso'),Request::post('imc'),Request::post('antecedentes'),Request::post('edadMaterna'),Request::post('paridad')));
        }else{
-        $this->View->renderJSON(InterconsultaModel::solicitarInterconsulta(Request::post('nombreReferente'), Request::post('correoReferente'), Request::post('nombre'), Request::post('rut'), Request::post('fecha'), Request::post('diagnostico'), Request::post('lugar'), Request::post('ciudad'), Request::post('profesional'), Request::post('nombreContrarreferente'), Request::post('correoContrarreferente'), Request::post('fum'), Request::post('eg'), Request::post('telefono'), Request::post('sistolica'), Request::post('diastolica'), Request::post('media'), Request::post('talla'), Request::post('peso'), Request::post('imc'), Request::post('antecedentes'), Request::post('edadMaterna'), Request::post('paridad')));
+        $this->View->renderJSON(InterconsultaModel::solicitarInterconsulta(
+            Request::post('nombreContrarreferente'),
+            Request::post('correoContrarreferente'),
+            Request::post('nombre'),
+            Request::post('rut'),
+            Request::post('fecha'),
+            Request::post('diagnostico'),
+            Request::post('lugar'),
+            Request::post('ciudad'),
+            Request::post('profesional'),
+            Request::post('nombreReferente'),
+            Request::post('correoReferente'),
+            Request::post('fum'),
+            Request::post('eg'),
+            Request::post('telefono'),
+            Request::post('sistolica'),
+            Request::post('diastolica'),
+            Request::post('media'),
+            Request::post('talla'),
+            Request::post('peso'),
+            Request::post('imc'),
+            Request::post('antecedentes'),
+            Request::post('edadMaterna'),
+            Request::post('paridad')
+        ));
        }
     }
 
