c2bb19de913541441906ec3795699b64d268a669
Cristopher Castro <cristophernic@live.cl>
2019-09-09 23:13:25 -0300

    .

--- application/model/GraphModel.php
+++ application/model/GraphModel.php
@@ -142,8 +142,8 @@ class GraphModel
         $settings["graph_title"] = 'Cráneo';
         $settings["axis_min_h"] = '12';
         $settings["axis_min_v"] = '30';
-        $settings["axis_max_v"] = '480';
-        $settings["grid_division_v"] = '160';
+        $settings["axis_max_v"] = '400';
+        $settings["grid_division_v"] = '140';
         $settings["grid_division_h"] = '7';
 
         $values = DataModel::cc();
