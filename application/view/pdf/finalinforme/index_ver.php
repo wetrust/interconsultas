568c6fd1e68eff437e31c2259e2b6169f56e07da
Cristopher Castro <cristophernic@live.cl>
2019-09-14 21:21:37 -0300

    titulo

--- application/view/pdf/finalinforme/index_ver.php
+++ application/view/pdf/finalinforme/index_ver.php
@@ -41,7 +41,7 @@
     $fecha = explode("-", $this->solicitud_resultado->fecha);
     $fecha = $fecha[2] . "-". $fecha[1]. "-". $fecha[0];
 
-    $html = '<h4 style="border-bottom:2px double #000;text-align: center;">RESUMEN PROTOCOLO DE REFERENCIA Y CONTRARREFERENCIA PARA ECOGRAFÍA OBSTÉTRICA Y FLUJOMETRÍA DOPPLER</h4>';
+    $html = '<h4 style="border-bottom:2px double #000;text-align: center;">PROTOCOLO PARA REFERENCIA Y CONTRARREFERENCIA DE EXÁMENES ECOGRÁFICOS Y FLUJOMETRÍA DOPPLER</h4>';
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'C', true);
     $this->pdf->Ln(2);
 
