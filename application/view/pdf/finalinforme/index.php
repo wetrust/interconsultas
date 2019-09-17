cfac1b5391ef4678072a2d2277edfb87c3c8567d
Cristopher Castro <cristophernic@live.cl>
2019-09-17 16:04:39 -0300

    .

--- application/view/pdf/finalinforme/index.php
+++ application/view/pdf/finalinforme/index.php
@@ -41,7 +41,7 @@
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'C', true);
     $this->pdf->Ln(2);
 
-    $html = '<p style="color:#0275d8;"><em>Datos de referencia para evaluación ecográfica.</em></p>';
+    $html = '<h4 style="color:#0275d8;"><em>Datos de referencia para evaluación ecográfica.</em></h4>';
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
     $this->pdf->Ln(2);
 
@@ -106,7 +106,7 @@
         $this->pdf->Ln(4);
     }
 
-    $html = '<p style="color:#0275d8;"><em>Resumen examen ecográfico y flujometría Doppler materno / fetal.</em></p>';
+    $html = '<h4 style="color:#0275d8;"><em>Resumen examen ecográfico y flujometría Doppler materno / fetal.</em></h4>';
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'L', true);
     $this->pdf->Ln(2);
 
@@ -161,7 +161,7 @@
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(8);
 
-    $html = '<table><tbody><tr><td style="width:450px"></td><td>Ecografista: '.htmlentities($this->solicitud_resultado->ecografista).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td style="width:450px"></td><td style="color:#0275d8;">Ecografista: '.htmlentities($this->solicitud_resultado->ecografista).'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(4);
     $html = '<table style="border-top:1px solid #000;border-bottom:1px solid #000;"><tbody><tr><td><p>Fecha de exámen: '. $fecha .'</p></td></tr></tbody></table>';
