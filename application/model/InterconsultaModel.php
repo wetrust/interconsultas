34f36905b30c0fb7d21ac77deaf539b8bb629780
Cristopher Castro <cristophernic@live.cl>
2019-09-24 20:03:30 -0300

    evaluacion

--- application/model/InterconsultaModel.php
+++ application/model/InterconsultaModel.php
@@ -30,8 +30,9 @@ class InterconsultaModel
         $solicitud_profesional = UserModel::getPublicProfileOfUser(Session::get('user_id'))->user_profesion;
         $solicitud_nombreprofesional = Session::get('user_name');
         $solicitud_email = Session::get('user_email');
-        $solicitud_respuesta = 0;
+        $solicitud_respuesta = 1;
         $solicitud_id = SolicitudesModel::createSolicitud($solicitud_nombre_referente, $solicitud_profesionalemail,$solicitud_nombre,$solicitud_rut,$solicitud_fecha,$solicitud_diagnostico,$solicitud_lugar,$solicitud_ciudad,$solicitud_profesional,$solicitud_nombreprofesional,$solicitud_email,$solicitud_fum,$solicitud_respuesta,$solicitud_egestacional, $telefono, $sistolica, $diastolica, $media, $talla, $peso, $imc, $antecedentes, $edadMaterna, $paridad);
+        EvaluacionModel::createEvaluacion($solicitud_id, $solicitud_fecha, "");
         $response->result = SolicitudesModel::confirmarSolicitud($solicitud_id);
         return $response;
     }
