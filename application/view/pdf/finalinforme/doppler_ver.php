5fa6cea38aea0bcb41e2de4567002bd8332b2e1c
Cristopher Castro <cristophernic@live.cl>
2019-10-06 21:43:37 -0300

    .

--- application/view/pdf/finalinforme/doppler_ver.php
+++ application/view/pdf/finalinforme/doppler_ver.php
@@ -125,7 +125,7 @@
     $html = '<table><tbody><tr><td><strong><em>Descripción</em></strong></td><td>Embrión: '. htmlentities($this->respuesta_embrion) .'</td><td></td><td>Frecuencia cardiaca fetal: '. htmlentities($this->respuesta_fcf) .'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
-    $html = '<table><tbody><tr><td style="width:160px"></td><td>Anatomía fetal: '.htmlentities($this->respuesta_anatomia). ' '.htmlentities($this->respuesta_anatomia_extra).'</td></tr></tbody></table>';
+    $html = '<table><tbody><tr><td style="width:163px"></td><td>Anatomía fetal: '.htmlentities($this->respuesta_anatomia). ' '.htmlentities($this->respuesta_anatomia_extra).'</td></tr></tbody></table>';
     $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(4);
     $html = '<table><tbody><tr><td><strong><em>Biometría ecográfica</em></strong></td><td>LCN: *</td><td>'.htmlentities($this->respuesta_lcn).' mm.</td><td>EG x LCN: '.htmlentities($this->respuesta_lcn_eg).'</td></tr></tbody></table>';
