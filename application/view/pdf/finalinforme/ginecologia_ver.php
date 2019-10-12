fbd79d346de919be12a137c64c0d40c7bf9b2a73
Cristopher Castro <cristophernic@live.cl>
2019-10-12 01:21:03 -0300

    .

--- application/view/pdf/finalinforme/ginecologia_ver.php
+++ application/view/pdf/finalinforme/ginecologia_ver.php
@@ -38,7 +38,7 @@
     $solicitud_fum = explode("-", $this->solicitud->solicitud_fum);
     $solicitud_fum = $solicitud_fum[2] . "-". $solicitud_fum[1]. "-". $solicitud_fum[0];
 
-    $html = '<h4 style="border-bottom:2px double #000;text-align: center;">RESUMEN PROTOCOLO DE REFERENCIA Y CONTRARREFERENCIA PARA ECOGRAFÍA GINECOLÓGICA</h4>';
+    $html = '<h3>RESUMEN PROTOCOLO DE REFERENCIA Y CONTRARREFERENCIA PARA ECOGRAFÍA GINECOLÓGICA</h3>';
     $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'C', true);
     $this->pdf->Ln(2);
 
