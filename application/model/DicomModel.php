e3ef3cb1283464a254bd59442fc2e4b31b8fa65c
Cristopher Castro <cristophernic@live.cl>
2019-10-13 19:22:25 -0300

    .

--- application/model/DicomModel.php
+++ application/model/DicomModel.php
@@ -6,7 +6,7 @@ class DicomModel
     {
             $database = "";
             $result = new stdClass();
-            $StudyDate = intval($StudyInsta);
+
             try {
                 $options = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ, PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING);
                 $database = new PDO(
