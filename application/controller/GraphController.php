1910f7a80306ae5dd691d3851c3c5e810b5c4198
Cristopher Castro <cristophernic@live.cl>
2019-10-12 22:03:13 -0300

    .

--- application/controller/GraphController.php
+++ application/controller/GraphController.php
@@ -2,13 +2,11 @@
 
 class GraphController extends Controller
 {
-    public function __construct()
-    {
+    public function __construct(){
         parent::__construct();
     }
 
-    public function informe_dopplercrecimiento($solicitud_id)
-    {
+    public function informe_dopplercrecimiento($solicitud_id){
         $respuesta = RespuestaModel::getRespuesta($solicitud_id);
         $respuesta->eg = str_replace(" semanas", "", $respuesta->eg);
         $respuesta->eg = explode (".", $respuesta->eg);
@@ -35,29 +33,34 @@ class GraphController extends Controller
         ));
     }
 
-    public function informe_segundotrimestre($solicitud_id)
-    {
+    public function informe_segundotrimestre($solicitud_id){
         $respuesta = RespuestaModel::getRespuesta($solicitud_id);
         $respuesta->eg = str_replace(" semanas", "", $respuesta->eg);
         $respuesta->eg = explode (".", $respuesta->eg);
         $respuesta->eg = $respuesta->eg[0];
+
+        $grafico_uno = ($respuesta->cc > 0) ? array($respuesta->eg => $respuesta->cc) : array();
+        $grafico_dos = ($respuesta->ca > 0) ? array($respuesta->eg => $respuesta->ca) : array();
+        $grafico_tres = ($respuesta->lf > 0) ? array($respuesta->eg => $respuesta->lf) : array();
+        $grafico_cuatro = ($respuesta->respuesta_lh > 0) ? array($respuesta->eg => $respuesta->respuesta_lh) : array();
+        $grafico_cinco = ($respuesta->pfe_segundo > 0) ? array($respuesta->eg => $respuesta->pfe_segundo) : array();
+        $grafico_seis = ($respuesta->ccca > 0) ? array($respuesta->eg => $respuesta->ccca) : array();
 
         $this->View->renderWithoutHeaderAndFooter('pdf/finalinforme/segundotrimestre_grafico_ver', 
         array(
             'pdf' => new PdfModel(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false),
             'solicitud' => SolicitudesModel::getSolicitud($solicitud_id),
             'respuesta' => $respuesta,
-            'grafico_uno' => GraphModel::cc(array($respuesta->eg => $respuesta->cc)),
-            'grafico_dos' => GraphModel::ca(array($respuesta->eg => $respuesta->ca)),
-            'grafico_tres' => GraphModel::lf(array($respuesta->eg => $respuesta->lf)),
-            'grafico_cuatro' => GraphModel::lh(array($respuesta->eg => $respuesta->respuesta_lh)),
-            'grafico_cinco' => GraphModel::pesoFetal(array($respuesta->eg => $respuesta->pfe_segundo)),
-            'grafico_seis' => GraphModel::ccca(array($respuesta->eg => $respuesta->ccca)),
+            'grafico_uno' => GraphModel::cc($grafico_uno),
+            'grafico_dos' => GraphModel::ca($grafico_dos),
+            'grafico_tres' => GraphModel::lf($grafico_tres),
+            'grafico_cuatro' => GraphModel::lh($grafico_cuatro),
+            'grafico_cinco' => GraphModel::pesoFetal($grafico_cinco),
+            'grafico_seis' => GraphModel::ccca($grafico_seis),
         ));
     }
 
-    public function informe_parto($solicitud_id)
-    {
+    public function informe_parto($solicitud_id){
         $respuesta = PartosModel::getPartos($solicitud_id);
         $paciente = SolicitudesModel::getOldSolicitudes($solicitud_id);
 
