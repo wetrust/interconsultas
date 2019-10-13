1e989d1859cda059f0239e78ecc1af56ebd8a64f
Cristopher Castro <cristophernic@live.cl>
2019-10-12 21:51:51 -0300

    .

--- application/controller/GraphController.php
+++ application/controller/GraphController.php
@@ -14,17 +14,24 @@ class GraphController extends Controller
         $respuesta->eg = explode (".", $respuesta->eg);
         $respuesta->eg = $respuesta->eg[0];
 
+        $grafico_uno = ($respuesta->pfe > 0) ? array($respuesta->eg => $respuesta->pfe) : array();
+        $grafico_dos = ($respuesta->ca > 0) ? array($respuesta->eg => $respuesta->ca) : array();
+        $grafico_tres = ($respuesta->uterinas > 0) ? array($respuesta->eg => $respuesta->uterinas) : array();
+        $grafico_cuatro = ($respuesta->umbilical > 0) ? array($respuesta->eg => $respuesta->umbilical) : array();
+        $grafico_cinco = ($respuesta->cm > 0) ? array($respuesta->eg => $respuesta->cm) : array();
+        $grafico_seis = ($respuesta->cmau > 0) ? array($respuesta->eg => $respuesta->cmau) : array();
+
         $this->View->renderWithoutHeaderAndFooter('pdf/finalinforme/index_grafico_ver', 
         array(
             'pdf' => new PdfModel(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false),
             'solicitud' => SolicitudesModel::getSolicitud($solicitud_id),
             'respuesta' => $respuesta,
-            'grafico_uno' => GraphModel::pesoFetal(array($respuesta->eg => $respuesta->pfe)),
-            'grafico_dos' => GraphModel::ca(array($respuesta->eg => $respuesta->ca)),
-            'grafico_tres' => GraphModel::uterinas(array($respuesta->eg => $respuesta->uterinas)),
-            'grafico_cuatro' => GraphModel::umbilical(array($respuesta->eg => $respuesta->umbilical)),
-            'grafico_cinco' => GraphModel::cerebralMedia(array($respuesta->eg => $respuesta->cm)),
-            'grafico_seis' => GraphModel::cuocienteCerebroPlacentario(array($respuesta->eg =>$respuesta->cmau)),
+            'grafico_uno' => GraphModel::pesoFetal($grafico_uno),
+            'grafico_dos' => GraphModel::ca($grafico_dos),
+            'grafico_tres' => GraphModel::uterinas($grafico_tres),
+            'grafico_cuatro' => GraphModel::umbilical($grafico_cuatro),
+            'grafico_cinco' => GraphModel::cerebralMedia($grafico_cinco),
+            'grafico_seis' => GraphModel::cuocienteCerebroPlacentario($grafico_seis),
         ));
     }
 
