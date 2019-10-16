fcf7555346f8f0ccc456e2b0fd1fffb175d158b6
Cristopher Castro <cristophernic@live.cl>
2019-10-15 22:11:00 -0300

    .

--- application/view/pdf/finalinforme/imagen.php
+++ application/view/pdf/finalinforme/imagen.php
@@ -33,33 +33,33 @@
 
 
     if (count($this->user_images) != 4){
-        $pdf->Ln(6);
+        $this->pdf->Ln(6);
     }
     $html = '<h3 style="border-bottom:2px double #000;text-align: center;"><strong>Impresión de Imágenes Gineco-Obstétrica</strong></h3>';
-    $pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'C', true);
-    $pdf->Ln(4);
+    $this->pdf->writeHTMLCell('', '', '10', '', $html, 0, 1, 0, true, 'C', true);
+    $this->pdf->Ln(4);
     
     // Image example with resizing
     if (count($this->user_images) == 1){
-        $pdf->Image($this->user_images[0], '', '', 180, 140, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[0], '', '', 180, 140, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
     }
     else if (count($this->user_images) == 2){
-    $pdf->Image($this->user_images[0], '60', '', 100, 88, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-    $pdf->Image($this->user_images[1], '60', 170, 100, 88, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[0], '60', '', 100, 88, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[1], '60', 170, 100, 88, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
     }
     else if (count($this->user_images) == 4){
-        $pdf->Image($this->user_images[0], PDF_MARGIN_LEFT+10, '', 106, 74, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-        $pdf->Image($this->user_images[1], '140', '', 106, 74, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-        $pdf->Image($this->user_images[2], PDF_MARGIN_LEFT+10, '120', 106, 74, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-        $pdf->Image($this->user_images[3], '140', '', 106, 74, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[0], PDF_MARGIN_LEFT+10, '', 106, 74, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[1], '140', '', 106, 74, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[2], PDF_MARGIN_LEFT+10, '120', 106, 74, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[3], '140', '', 106, 74, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
     }
     else if (count($this->user_images) == 6){
-        $pdf->Image($this->user_images[0], '', '', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-        $pdf->Image($this->user_images[1], '110', '', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-        $pdf->Image($this->user_images[2], PDF_MARGIN_LEFT, '115', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-        $pdf->Image($this->user_images[3], '110', '', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-        $pdf->Image($this->user_images[4], PDF_MARGIN_LEFT, '186', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
-        $pdf->Image($this->user_images[5], '110', '', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[0], '', '', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[1], '110', '', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[2], PDF_MARGIN_LEFT, '115', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[3], '110', '', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[4], PDF_MARGIN_LEFT, '186', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
+        $this->pdf->Image($this->user_images[5], '110', '', 88, 63, 'JPG', '', 'T', true, 150, '', false, false, 1, false, false, false);
     }
 
     $base64 = chunk_split(base64_encode($this->pdf->Output('Informe.pdf', 'S')));
