39f958d697e56d77459788c8423d583a937abdf9
Cristopher Castro <cristophernic@live.cl>
2019-10-23 23:52:14 -0300

    .

--- application/view/dashboard/pacientes.php
+++ application/view/dashboard/pacientes.php
@@ -2,4 +2,4 @@
     <h2>Pacientes</h2>
     <div class="card shadow"><div class="card-body" id="pacientes"></div></div>
 </div>
-<script type="module" src="js/pacientes/app.js"></script>
+<script src="js/pacientes/app.js"></script>
