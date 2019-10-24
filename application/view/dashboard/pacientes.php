70f5b4a55d4ed64733ed4f0201d813a7566c106e
Cristopher Castro <cristophernic@live.cl>
2019-10-23 22:08:04 -0300

    .

--- application/view/dashboard/pacientes.php
+++ application/view/dashboard/pacientes.php
@@ -1,1 +1,9 @@
-<p>Pacientes is work</p>
+<div class="container">
+    <h5>Pacientes</h5>
+    <div class="text-center">
+    <div class="spinner-grow" style="width: 3rem; height: 3rem;" role="status">
+        <span class="sr-only">Cargando...</span>
+    </div>
+    <p>Cargando..</p>
+    </div>
+</div>
