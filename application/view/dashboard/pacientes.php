d4f2c86aa74662dd1ee8cfcd2a4a01baaf2a2d67
Cristopher Castro <cristophernic@live.cl>
2019-10-25 23:35:43 -0300

    .

--- application/view/dashboard/pacientes.php
+++ application/view/dashboard/pacientes.php
@@ -1,5 +1,8 @@
 <div class="container">
-    <h2>Pacientes</h2>
+    <div class="d-flex justify-content-between align-items-center rounded px-3 py-1 shadow-sm" style="background-color: #e9ecef">
+        <h5 class="text-primary mb-0"><em>Pacientes</em></h5>
+        <a href="<?php echo Config::get('URL'); ?>"><strong>Volver</strong></a>
+    </div>
     <div class="card shadow"><div class="card-body" id="pacientes"></div></div>
 </div>
 <script src="js/jquery.rut.chileno.min.js"></script>
