159f2b86bf9552f51013388323bf8b1194667808
Cristopher Castro <cristophernic@live.cl>
2019-10-30 20:17:46 -0300

    añadir botón modificar

--- public/js/basico.js
+++ public/js/basico.js
@@ -177,8 +177,9 @@ function createCarcasaInterconsultaModal(id){
     modal_id = uuidv4();
     div_id = uuidv4();
     btn_id = uuidv4();
+    btn_modificar_id = uuidv4();
 
-    var footerModal = '</div><div class="modal-footer"><button type="button" class="btn btn-danger" id="'+btn_id+'" data-id="'+id+'" data-modal="'+modal_id+'">Eliminar solicitud</button><button type="button" class="btn btn-secondary" data-dismiss="modal">Volver</button></div></div></div></div>';
+    var footerModal = '</div><div class="modal-footer"><button type="button" class="btn btn-danger" id="'+btn_modificar_id+'" data-id="'+id+'" data-modal="'+modal_id+'">Modificar solicitud</button><button type="button" class="btn btn-danger" id="'+btn_id+'" data-id="'+id+'" data-modal="'+modal_id+'">Eliminar solicitud</button><button type="button" class="btn btn-secondary" data-dismiss="modal">Volver</button></div></div></div></div>';
     $('body').append('<div class="modal" tabindex="-1" role="dialog" id="'+modal_id+'"> <div class="modal-dialog modal-lg" role="document"> <div class="modal-content"> <div class="modal-header"> <h5 class="modal-title">Interconsulta</h5></div><div class="modal-body"><div class="row" id="'+div_id+'"><div class="progress col-12 my-4"><div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%"><strong>CARGANDO</strong></div></div></div>'+ footerModal);
 
     $('#'+modal_id).modal("show").on('hidden.bs.modal', function (e) {
