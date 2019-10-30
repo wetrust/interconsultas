f7593ca5f954de543de847c0d1fb86b7a0eb6812
Cristopher Castro <cristophernic@live.cl>
2019-10-30 16:22:23 -0300

    .

--- application/view/pdf/finalinforme/segundotrimestre_ver.php
+++ application/view/pdf/finalinforme/segundotrimestre_ver.php
@@ -55,9 +55,6 @@
         $html = '<table><tbody><tr><td>Fecha solicitud de la ecografía</td><td>: '.$solicitud_fecha.'</td><td>FUR referida o corregida</td><td>: '.$solicitud_fum.'</td></tr></tbody></table>';
         $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
         $this->pdf->Ln(1);
-        $html = '<table><tbody><tr><td>Edad gestacional</td><td>: '.htmlentities($this->solicitud->solicitud_egestacional).'</td></tr></tbody></table>';
-        $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
-        $this->pdf->Ln(1);
         $html = '<table><tbody><tr><td>Motivo de exámen</td><td>: '.htmlentities($this->solicitud->solicitud_diagnostico).'</td></tr></tbody></table>';
         $this->pdf->writeHTMLCell('', '', '', '', $html, 0, 1, 0, true, 'J', true);
         if (property_exists($this,"uterinas")){
