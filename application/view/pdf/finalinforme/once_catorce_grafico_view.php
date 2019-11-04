f31ade65ef75601695eb888a0f4976297f7f7797
Cristopher Castro <cristophernic@live.cl>
2019-11-03 23:13:57 -0300

    .

--- application/view/pdf/finalinforme/once_catorce_grafico_view.php
+++ application/view/pdf/finalinforme/once_catorce_grafico_view.php
@@ -40,8 +40,14 @@
     $this->pdf->ImageSVG('@' . $this->grafico_tres, $x=10, $y=105, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
     $this->pdf->ImageSVG('@' . $this->grafico_cuatro, $x=105, $y=105, $w='', $h=90, $link='', $align='', $palign='', $border=0, $fitonpage=false);
 
+    $fecha = explode("-", $this->respuesta_fecha);
+    $fecha = $fecha[2] . "-". $fecha[1]. "-". $fecha[0];
+    
+    $html = '<table style="border-top:1px solid #000;border-bottom:1px solid #000;"><tbody><tr><td><p>Fecha de exámen: '. $fecha .'</p></td></tr></tbody></table>';
+    $this->pdf->writeHTMLCell('', '', $x=10, $y=185,, $html, 0, 1, 0, true, 'L', true);
+    $this->pdf->Ln(1);
     $html = '<p><small>* Referencia para LCN Hadlock FP, Shan YP, Kanon JD y cols.: Radiology 182:501, 1992.<br>** Referencia para Doppler promedio de arterias uterinas: Gómes O., Figueras F., Fernandez S., Bennasar M, Martínez JM., Puerto B., Gratacos E., UOG 2008; 32: 128-32<br>*** Referencia para biometrías según gráfica de Hadlock y col. 1984</small></p>';
-    $this->pdf->writeHTMLCell('', '', $x=10, $y=185, $html, 0, 1, 0, true, 'J', true);
+    $this->pdf->writeHTMLCell('', '', $x=10, $y=187, $html, 0, 1, 0, true, 'J', true);
     $this->pdf->Ln(1);
 
     
