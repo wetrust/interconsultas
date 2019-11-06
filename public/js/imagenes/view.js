916d8a1665c4a3c84047dee670f640cdee0e0def
Cristopher Castro <cristophernic@live.cl>
2019-11-05 22:44:29 -0300

    declaracion

--- public/js/imagenes/view.js
+++ public/js/imagenes/view.js
@@ -4,7 +4,7 @@ export class view {
     static imagenesInterface(container, data){
         let estructura = '<div class="row">';
             
-        for (i in data.JPGFiles) {
+        for (let i in data.JPGFiles) {
             estructura += '<div class="col-12 col-lg-3 col-md-4 col-sm-6"><a href="https://servidor.crecimientofetal.cl/data/'+ data.JPGFiles[i][1] +'" target="_blank"><img alt="Imágen ecográfica" src="https://servidor.crecimientofetal.cl/data/'+ data.JPGFiles[i][1] +'" class="img-fluid border border-primary rounded shadow mb-2" /></a><div class="form-group form-check"><input type="checkbox" class="form-check-input" name="foto" data-foto="'+ data.JPGFiles[i][1] +'"><label class="form-check-label">Seleccionar</label></div></div>';
         }
 
