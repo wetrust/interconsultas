3f279647376b50f94507538087ae156f373dd475
Cristopher Castro <cristophernic@live.cl>
2019-11-13 23:12:03 -0300

    .

--- public/js/imagenes/config.js
+++ public/js/imagenes/config.js
@@ -4,5 +4,5 @@ export const config = {
     ciudadesProfesional: 'dashboard/getCiudadesProfesional/',
     emailProfesional: 'dashboard/getEmailProfesional/',
     emailSend: 'dashboard/emailCompleto',
-    imagenesInterface: '<div class="d-flex flex-row mb-3"><button type="button" class="btn btn-primary d-none" id="imagenes.impresion">Ver imágenes</button><button type="button" class="btn btn-primary d-none ml-1" id="imagenes.email">Enviar por e-mail</button></div><div class="row">'
+    imagenesInterface: '<div class="d-flex flex-row mb-3"><button type="button" class="btn btn-primary d-none" id="imagenes.impresion">Ver imágenes</button><button type="button" class="btn btn-primary d-none ml-1" id="imagenes.email">Enviar por E-mail</button></div><div class="row">'
 }
