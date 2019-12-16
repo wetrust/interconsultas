a6f4dd059f1bfa05392f3ef5cae2d4a9e17ffd69
Cristopher Castro <cristophernic@live.cl>
2019-12-16 20:07:40 -0300

    .

--- public/js/configuracion.js
+++ public/js/configuracion.js
@@ -10,6 +10,8 @@ $(document).ready(function(){
 
     $("#directorio\\.nuevo").on("click", function(){createCarcasaDirectorio();});
     $("#diagnostico\\.nuevo").on("click", function(){createCarcasaDiagnostico();});
+
+    $("#parto\\.autorizar").on("click", function(){createCarcasaAutorizar();});
 });
 
 function loadDirectorio(){
@@ -232,6 +234,36 @@ function createCarcasaDiagnostico(){
         });
 
         let dav = {diagnostico_name: $("#modal\\.diagnostico\\.nombre").val()}
+        
+        $.post('dashboard/diagnosticoSave', dav).done(function(data){
+            $('#'+modal_id).modal("hide"); $('#mensaje\\.dialogo').modal("hide"); loadDiagnosticoConfiguracion(); loadDiagnostico();
+        });
+    });
+}
+
+function createCarcasaAutorizar(){
+    var modal_id, div_id;
+
+    modal_id = uuidv4();
+    div_id = uuidv4();
+    btn_responder_id = uuidv4();
+
+    var footerModal = '</div><div class="modal-footer"><button id="'+btn_responder_id+'" data-modal="'+modal_id+'" class="btn btn-primary">Guardar</button><button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button></div></div></div></div>';
+    $('body').append('<div class="modal" tabindex="-1" role="dialog" id="'+modal_id+'"> <div class="modal-dialog modal-lg" role="document"> <div class="modal-content"> <div class="modal-header"> <h5 class="modal-title">Autorizar a un usuario parto</h5></div><div class="modal-body"> <div class="row" id="'+div_id+'"> <div class="col-4 form-group"> <label>Nombre del Motivo de exámen</label> <input type="text" class="form-control" id="modal.diagnostico.nombre"> </div></div>'+ footerModal);
+
+    $('#'+modal_id).modal("show").on('hidden.bs.modal', function (e) {
+        $(this).remove();
+    });
+
+    $('#'+btn_responder_id).on("click", function(){
+        var modal_id = $(this).data("modal");
+        
+        $('body').append('<div class="modal" tabindex="-1" role="dialog" id="mensaje.dialogo"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">Enviando Datos</h5></div><div class="modal-body"><h3 class="text-danger text-center">Guardando, espere...</h3></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button></div></div></div></div>');
+        $('#mensaje\\.dialogo').modal("show").on('hidden.bs.modal', function (e) {
+            $(this).remove();
+        });
+
+        let dav = {autorizar_name: $("#modal\\.diagnostico\\.nombre").val()}
         
         $.post('dashboard/diagnosticoSave', dav).done(function(data){
             $('#'+modal_id).modal("hide"); $('#mensaje\\.dialogo').modal("hide"); loadDiagnosticoConfiguracion(); loadDiagnostico();
