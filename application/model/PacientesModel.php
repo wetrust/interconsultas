88999a7b390b087d1c7c64df4e441562b68d949f
Cristopher Castro <cristophernic@live.cl>
2019-12-15 23:25:17 -0300

    .

--- application/model/PacientesModel.php
+++ application/model/PacientesModel.php
@@ -6,7 +6,7 @@ class PacientesModel
     {
         $database = DatabaseFactory::getFactory()->getConnection();
 
-        $sql = "SELECT id, nombre, apellido, rut, fum, ciudad, lugar, telefono FROM pacientes WHERE user_id = :user_id ORDER BY rut";
+        $sql = "SELECT id, nombre, apellido, rut, fum, ciudad, lugar, telefono FROM pacientes WHERE user_id = :user_id ORDER BY apellido";
         $query = $database->prepare($sql);
         $query->execute(array(':user_id' => Session::get('user_id')));
 
