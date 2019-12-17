f0adc7b9d22fe4eff99f8df05775808c0ad32f62
Cristopher Castro <cristophernic@live.cl>
2019-12-16 22:09:22 -0300

    .

--- application/view/dashboard/parto.php
+++ application/view/dashboard/parto.php
@@ -1,7 +1,31 @@
 <div class="container">
     <div class="card">
         <div class="card-body">
-            <h5 class="card-title">Ecografías realizadas</h5>
+            <h5 class="card-title">Parto y Recién nacido</h5>
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="espera-tab" data-toggle="tab" href="#espera" role="tab" aria-controls="espera" aria-selected="true">En espera de parto</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="partos-tab" data-toggle="tab" href="#partos" role="tab" aria-controls="partos" aria-selected="false">Partos atendidos</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="ajuste-tab" data-toggle="tab" href="#ajuste" role="tab" aria-controls="ajuste" aria-selected="false">Ajuste al peso neonatal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="protocolo-tab" data-toggle="tab" href="#protocolo" role="tab" aria-controls="protocolo" aria-selected="false">Protocolo de hipoglicemia</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="espera" role="tabpanel" aria-labelledby="espera-tab">
+                </div>
+                <div class="tab-pane fade" id="partos" role="tabpanel" aria-labelledby="partos-tab">
+                </div>
+                <div class="tab-pane fade" id="ajuste" role="tabpanel" aria-labelledby="ajuste-tab">
+                </div>
+                <div class="tab-pane fade" id="protocolo" role="tabpanel" aria-labelledby="protocolo-tab">
+                </div>
+            </div>
         </div>
     </div>
 </div>
