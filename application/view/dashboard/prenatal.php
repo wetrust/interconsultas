7b1ed2ca3dde6a31868de32cb9c8d74fbdd7337a
Cristopher Castro <cristophernic@live.cl>
2019-12-16 22:02:13 -0300

    .

--- application/view/dashboard/prenatal.php
+++ application/view/dashboard/prenatal.php
@@ -2,6 +2,21 @@
     <div class="card">
         <div class="card-body">
             <h5 class="card-title">Ecografías realizadas</h5>
+            <table class="table table-hover">
+                <thead class="thead-dark">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Tipo de exámen</th>
+                        <th scope="col">Realizado</th>
+                        <th scope="col">Edad Gest.</th>
+                        <th scope="col">Nombre paciente</th>
+                        <th scope="col">R.U.T.</th>
+                        <th scope="col">Accion</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
