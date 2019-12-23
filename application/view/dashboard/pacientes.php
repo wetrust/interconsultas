f01778524d27f5e1e9887a13a2d72d274a63f744
Cristopher Castro <cristophernic@live.cl>
2019-12-23 03:23:51 -0300

    volver

--- application/view/dashboard/pacientes.php
+++ application/view/dashboard/pacientes.php
@@ -1,7 +1,7 @@
 <div class="container">
     <div class="d-flex justify-content-between align-items-center rounded px-3 py-1 shadow-sm" style="background-color: #e9ecef">
         <h5 class="text-primary mb-0"><em>Pacientes</em></h5>
-        <a href="<?php echo Config::get('URL'); ?>"><strong>Volver</strong></a>
+        <a href="<?php echo Config::get('URL'); ?>/dashboard/sistema#interconsulta"><strong>Volver</strong></a>
     </div>
     <div class="card shadow mt-3"><div class="card-body" id="pacientes"></div></div>
 </div>
