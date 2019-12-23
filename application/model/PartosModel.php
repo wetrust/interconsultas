72654ef880de130cc336b3086ee205e29c7270cc
Cristopher Castro <cristophernic@live.cl>
2019-12-23 02:50:38 -0300

    .

--- application/model/PartosModel.php
+++ application/model/PartosModel.php
@@ -47,34 +47,7 @@ class PartosModel
 
     public static function updatePartos($parto_id,$fecha_parto, $semanas, $dias, $peso, $talla, $imc, $estado_nutricional, $etnia, $paridad, $lugar, $pesofetal, $tallafetal, $craneofetal, $apgar_uno, $apgar_cinco, $sexo, $meconio, $ipn, $peso_eg, $peso_eg_estado, $ipn_eg, $ipn_eg_estado, $comentarios, $hipoglicemia, $alta, $protocolo_hipoglicemia, $edad_materna) {
         $database = DatabaseFactory::getFactory()->getConnection();
-        $sql = "UPDATE partos SET 
-        fecha_parto = :fecha_parto,
-        semanas = :semanas,
-        dias = :dias,
-        peso = :peso,
-        talla = :talla,
-        imc = :imc, 
-        estado_nutricional = :estado_nutricional,
-        etnia = :etnia,
-        paridad = :paridad,
-        lugar = :lugar,
-              pesofetal = :pesofetal,
-                   tallafetal = :tallafetal,
-                    craneofetal = :craneofetal,
-                     apgar_uno = :apgar_uno,
-                      apgar_cinco = :apgar_cinco,
-                       sexo = :sexo,
-                        meconio = :meconio,
-                         ipn = :ipn,
-                          peso_eg = :peso_eg,
-                           peso_eg_estado = :peso_eg_estado,
-                            ipn_eg = :ipn_eg,
-                             ipn_eg_estado = :ipn_eg_estado,
-                              comentarios = :comentarios,
-                              hipoglicemia = :hipoglicemia,
-                              alta = :alta,
-                               protocolo_hipoglicemia = :protocolo_hipoglicemia,
-                                edad_materna = edad_materna WHERE parto_id = :parto_id";
+        $sql = "UPDATE partos SET fecha_parto = :fecha_parto, semanas = :semanas, dias = :dias, peso = :peso, talla = :talla, imc = :imc, estado_nutricional = :estado_nutricional, etnia = :etnia, paridad = :paridad, lugar = :lugar, pesofetal = :pesofetal, tallafetal = :tallafetal, craneofetal = :craneofetal, apgar_uno = :apgar_uno, apgar_cinco = :apgar_cinco, sexo = :sexo, meconio = :meconio, ipn = :ipn, peso_eg = :peso_eg, peso_eg_estado = :peso_eg_estado, ipn_eg = :ipn_eg, ipn_eg_estado = :ipn_eg_estado, comentarios = :comentarios, hipoglicemia = :hipoglicemia, alta = :alta, protocolo_hipoglicemia = :protocolo_hipoglicemia, edad_materna = :edad_materna WHERE parto_id = :parto_id";
         $query = $database->prepare($sql);
         $query->execute(array(':parto_id' => $parto_id, ':fecha_parto' => $fecha_parto,':semanas' => $semanas,':dias' => $dias,':peso' => $peso,':talla' => $talla,':imc' => $imc,':estado_nutricional' => $estado_nutricional,':etnia' => $etnia,':paridad' => $paridad,':lugar' => $lugar,':pesofetal' => $pesofetal,':tallafetal' => $tallafetal,':craneofetal' => $craneofetal,':apgar_uno' => $apgar_uno,':apgar_cinco' => $apgar_cinco,':sexo' => $sexo,':meconio' => $meconio,':ipn' => $ipn,':peso_eg' => $peso_eg,':peso_eg_estado' => $peso_eg_estado,':ipn_eg' => $ipn_eg,':ipn_eg_estado' => $ipn_eg_estado,':comentarios' => $comentarios,':hipoglicemia' => $hipoglicemia,':alta' => $alta, ':protocolo_hipoglicemia' => $protocolo_hipoglicemia, ':edad_materna' => $edad_materna));
         if ($query->rowCount() == 1) { 
