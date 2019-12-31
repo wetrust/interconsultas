d291950b754cdba0173e09c559e40147f358a227
Cristopher Castro <cristophernic@live.cl>
2019-12-30 21:05:02 -0300

    .

--- application/view/parto/index.php
+++ application/view/parto/index.php
@@ -7,8 +7,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<link rel="stylesheet" href="styles.3ff695c00d717f2d2a11.css"></head>
+  <link rel="stylesheet" href="styles.3ff695c00d717f2d2a11.css"></head>
 <body>
   <app-root></app-root>
-<script src="runtime-es2015.d61a8606b6095ee1d9a8.js" type="module"></script><script src="runtime-es5.d61a8606b6095ee1d9a8.js" nomodule defer></script><script src="polyfills-es5.6696c533341b95a3d617.js" nomodule defer></script><script src="polyfills-es2015.2987770fde9daa1d8a2e.js" type="module"></script><script src="main-es2015.ff0a8806dcc87a80db10.js" type="module"></script><script src="main-es5.ff0a8806dcc87a80db10.js" nomodule defer></script></body>
+  <script src="runtime-es2015.53e3db09b8d71688a2ef.js" type="module"></script><script src="runtime-es5.53e3db09b8d71688a2ef.js" nomodule defer></script><script src="polyfills-es5.7d7d156430c9cd92193e.js" nomodule defer></script><script src="polyfills-es2015.79b5019f4dff069483c4.js" type="module"></script><script src="main-es2015.c0983e6f69d9be718ccd.js" type="module"></script><script src="main-es5.c0983e6f69d9be718ccd.js" nomodule defer></script></body>
 </html>
