4996688ade55f348c34b6455793e9ff26641ac7b
Cristopher Castro <cristophernic@live.cl>
2020-01-02 20:37:57 -0300

    ups

--- application/view/parto/index.php
+++ application/view/parto/index.php
@@ -10,5 +10,5 @@
   <link rel="stylesheet" href="styles.3ff695c00d717f2d2a11.css"></head>
 <body>
   <app-root></app-root>
-  <script src="runtime-es2015.4f0fcfed5780565f57f5.js" type="module"></script><script src="runtime-es5.4f0fcfed5780565f57f5.js" nomodule defer></script><script src="polyfills-es5.890b05459dbcb2295382.js" nomodule defer></script><script src="polyfills-es2015.dafe66c02b6aff65605e.js" type="module"></script><script src="main-es2015.76e3383b710a454745fd.js" type="module"></script><script src="main-es5.76e3383b710a454745fd.js" nomodule defer></script></body>
+  <script src="runtime-es2015.9bb5084c9efa0694f4a1.js" type="module"></script><script src="runtime-es5.9bb5084c9efa0694f4a1.js" nomodule defer></script><script src="polyfills-es5.890b05459dbcb2295382.js" nomodule defer></script><script src="polyfills-es2015.dafe66c02b6aff65605e.js" type="module"></script><script src="main-es2015.76e3383b710a454745fd.js" type="module"></script><script src="main-es5.76e3383b710a454745fd.js" nomodule defer></script></body>
 </html>
