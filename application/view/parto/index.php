df737fdaaf0dc3d4348d2272f82be41674c1a722
Cristopher Castro <cristophernic@live.cl>
2020-01-06 18:39:29 -0300

    .

--- application/view/parto/index.php
+++ application/view/parto/index.php
@@ -1,14 +1,14 @@
 <!doctype html>
 <html lang="es">
-<head>
-  <meta charset="utf-8">
-  <title>Parto y Recién Nacido | Interconsultas</title>
-  <base href="https://administrador.crecimientofetal.cl/dataparto/">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="favicon.ico">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="styles.3ff695c00d717f2d2a11.css"></head>
-<body>
-  <app-root></app-root>
-  <script src="runtime-es2015.14305a4d2d31695939fe.js" type="module"></script><script src="runtime-es5.14305a4d2d31695939fe.js" nomodule defer></script><script src="polyfills-es5.890b05459dbcb2295382.js" nomodule defer></script><script src="polyfills-es2015.dafe66c02b6aff65605e.js" type="module"></script><script src="main-es2015.76e3383b710a454745fd.js" type="module"></script><script src="main-es5.76e3383b710a454745fd.js" nomodule defer></script></body>
+  <head>
+    <meta charset="utf-8"><title>Parto y Recién Nacido | Interconsultas</title>
+    <base href="https://administrador.crecimientofetal.cl/dataparto/"><meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.3ff695c00d717f2d2a11.css">
+  </head>
+  <body>
+    <app-root></app-root>
+    <script src="runtime-es2015.b6db9bfec6651a9f983e.js" type="module"></script><script src="runtime-es5.b6db9bfec6651a9f983e.js" nomodule defer></script><script src="polyfills-es5.890b05459dbcb2295382.js" nomodule defer></script><script src="polyfills-es2015.dafe66c02b6aff65605e.js" type="module"></script><script src="main-es2015.411fee1f660c46aa134c.js" type="module"></script><script src="main-es5.411fee1f660c46aa134c.js" nomodule defer></script>
+  </body>
 </html>
