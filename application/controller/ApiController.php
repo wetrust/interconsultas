288e968f5ef6e141a2d9ce3108757f6d6d965f55
Cristopher Castro <cristophernic@live.cl>
2020-01-08 00:21:18 -0300

    .

--- application/controller/ApiController.php
+++ application/controller/ApiController.php
@@ -180,7 +180,7 @@ class ApiController extends Controller{
             $protocolo_hipoglicemia = Request::post('protocolo_hipoglicemia');
             $edad_materna = Request::post('edad_materna');
             $pesoegcorregido = Request::post('pesoegcorregido');
-            $pesoegcorregidoestado = Request::post('pesoegcorregidoestado');
+            $pesoegcorregidoestado = Request::post('pesoegcorregidoEstado');
 
             $this->View->renderJSON(ApiModel::createPartos($token, $solicitud_id, $fecha_parto, $semanas, $dias, $peso, $talla, $imc, $estado_nutricional, $etnia, $paridad, $lugar, $pesofetal, $tallafetal, $craneofetal, $apgar_uno, $apgar_cinco, $sexo, $meconio, $ipn, $peso_eg, $peso_eg_estado, $ipn_eg, $ipn_eg_estado, $comentarios, $hipoglicemia, $alta,$protocolo_hipoglicemia, $edad_materna,$pesoegcorregido,$pesoegcorregidoestado));
         }
@@ -214,7 +214,7 @@ class ApiController extends Controller{
             $protocolo_hipoglicemia = Request::post('protocolo_hipoglicemia');
             $edad_materna = Request::post('edad_materna');
             $pesoegcorregido = Request::post('pesoegcorregido');
-            $pesoegcorregidoestado = Request::post('pesoegcorregidoestado');
+            $pesoegcorregidoestado = Request::post('pesoegcorregidoEstado');
             
             $this->View->renderJSON(ApiModel::updatePartos($token, $parto_id, $peso, $talla, $imc, $estado_nutricional, $etnia, $paridad, $lugar, $pesofetal, $tallafetal, $craneofetal, $apgar_uno, $apgar_cinco, $sexo, $meconio, $ipn, $peso_eg, $peso_eg_estado, $ipn_eg, $ipn_eg_estado, $comentarios, $hipoglicemia, $alta, $protocolo_hipoglicemia, $edad_materna,$pesoegcorregido,$pesoegcorregidoestado));
         }
