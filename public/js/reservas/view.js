662c77adba24ddcafdf2898c15ef9c231ac304dc
Cristopher Castro <cristophernic@live.cl>
2020-01-20 19:48:52 -0300

    .

--- public/js/reservas/view.js
+++ public/js/reservas/view.js
@@ -16,10 +16,9 @@ export class view {
         let modal = make.modal("Reservar hora");
         document.getElementsByTagName("body")[0].insertAdjacentHTML( 'beforeend', modal.modal);
         the(modal.titulo).innerHTML = config.newReservaTitulo;
+        the(modal.titulo).classList.add("mx-auto","text-white");
+        the(modal.titulo).parentElement.classList.add("g-verde");
         the(modal.contenido).innerHTML = config.newReservaHTML;
-
-        the(modal.id).children[0].classList.add("h-100","modal-xl");
-        the(modal.id).children[0].classList.remove("modal-lg");
         the(modal.contenido).classList.add("bg-light");
 
         $('#'+modal.id).modal("show").on('hidden.bs.modal', function (e) {
