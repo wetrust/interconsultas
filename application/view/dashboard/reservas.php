0f9037121cfa01f0d0944b65220ce4ab024a95e3
Cristopher Castro <cristophernic@live.cl>
2020-01-20 18:55:41 -0300

    reservas

--- application/view/dashboard/reservas.php
+++ application/view/dashboard/reservas.php
@@ -3,7 +3,7 @@
         <h5 class="text-primary mb-0"><em>Reservas</em></h5>
         <a href="<?php echo Config::get('URL'); ?>/"><strong>Volver</strong></a>
     </div>
-    <div class="card shadow mt-3"><div class="card-body" id="reservas"></div></div>
+    <div class="card shadow mt-3"><div class="card-body" id="reservas"><div class="text-center"><div class="spinner-grow" style="width: 3rem; height: 3rem;" role="status"><span class="sr-only">Cargando...</span></div><p>Cargando..</p></div></div></div>
 </div>
 <script src="js/jquery.rut.chileno.min.js"></script>
-<script type="module" src="js/pacientes/app.js"></script>
+<script type="module" src="js/reservas/app.js"></script>
