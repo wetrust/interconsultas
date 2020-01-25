faa6e842002cfedfcedead306bcac00c3c423d2e
Cristopher Castro <cristophernic@live.cl>
2020-01-25 00:23:27 -0300

    .

--- application/model/PreModel.php
+++ application/model/PreModel.php
@@ -14,16 +14,16 @@ class PreModel
         return $query->fetchAll();
     }
 
-    public static function getPre($data)
-    {
-        $database = DatabaseFactory::getFactory()->getConnection();
+    //public static function getPre($data)
+    //{
+    //    $database = DatabaseFactory::getFactory()->getConnection();
 
-        $sql = "SELECT user_id, pre_id, paciente_id, pre_fecha, pre_examen, pre_motivo FROM pre_examen WHERE user_id = :user_id AND pre_id = :pre_id LIMIT 1";
-        $query = $database->prepare($sql);
-        $query->execute(array(':user_id' => Session::get('user_id'), ':pre_id' => $data->id));
+    //    $sql = "SELECT user_id, pre_id, paciente_id, pre_fecha, pre_examen, pre_motivo FROM pre_examen WHERE user_id = :user_id AND pre_id = :pre_id LIMIT 1";
+    //    $query = $database->prepare($sql);
+    //    $query->execute(array(':user_id' => Session::get('user_id'), ':pre_id' => $data->id));
 
-        return $query->fetch();
-    }
+    //    return $query->fetch();
+    //}
 
     public static function createPre($data)
     {
