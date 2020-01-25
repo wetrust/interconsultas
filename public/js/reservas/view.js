f462500c8cd4c16a4f21b9c5720f5d55ff312b67
Cristopher Castro <cristophernic@live.cl>
2020-01-24 22:38:01 -0300

    actualizar

--- public/js/reservas/view.js
+++ public/js/reservas/view.js
@@ -211,7 +211,6 @@ export class view {
         the(modal.titulo).parentElement.classList.add("g-verde");
         the(modal.contenido).innerHTML = config.verPrepararHTML;
         the(modal.contenido).classList.add("bg-light");
-        the(modal.id).children[0].classList.add("h-100");
         $('#'+modal.id).modal("show").on('hidden.bs.modal', function (e) { $(this).remove(); });
 
         the("btn.dopcre").onclick = dopcre.interface;
