c775d4b0d03406454e8ddf1dabde1afcd674b56c
Cristopher Castro <cristophernic@live.cl>
2020-01-25 00:14:06 -0300

    nn

--- application/model/PreModel.php
+++ application/model/PreModel.php
@@ -39,44 +39,4 @@ class PreModel
 
         return false;
     }
-
-    public static function updateNote($note_id, $note_text)
-    {
-        if (!$note_id || !$note_text) {
-            return false;
-        }
-
-        $database = DatabaseFactory::getFactory()->getConnection();
-
-        $sql = "UPDATE notes SET note_text = :note_text WHERE note_id = :note_id AND user_id = :user_id LIMIT 1";
-        $query = $database->prepare($sql);
-        $query->execute(array(':note_id' => $note_id, ':note_text' => $note_text, ':user_id' => Session::get('user_id')));
-
-        if ($query->rowCount() == 1) {
-            return true;
-        }
-
-        Session::add('feedback_negative', Text::get('FEEDBACK_NOTE_EDITING_FAILED'));
-        return false;
-    }
-
-    public static function deleteNote($note_id)
-    {
-        if (!$note_id) {
-            return false;
-        }
-
-        $database = DatabaseFactory::getFactory()->getConnection();
-
-        $sql = "DELETE FROM notes WHERE note_id = :note_id AND user_id = :user_id LIMIT 1";
-        $query = $database->prepare($sql);
-        $query->execute(array(':note_id' => $note_id, ':user_id' => Session::get('user_id')));
-
-        if ($query->rowCount() == 1) {
-            return true;
-        }
-
-        Session::add('feedback_negative', Text::get('FEEDBACK_NOTE_DELETION_FAILED'));
-        return false;
-    }
 }
